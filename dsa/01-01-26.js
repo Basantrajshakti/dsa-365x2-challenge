f88a9a183e350a750358fd0f31f81cfc0aeeabbf
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-01 20:10:40 +0530

    Two Sum optimized

--- dsa/01-01-26.js
+++ dsa/01-01-26.js
@@ -6,18 +6,38 @@
  * @param {number} target
  * @return {number[]}
  */
+// var twoSum = function (nums, target) {
+//   const numsMap = {};
+
+//   for (let i = 0; i < nums.length; i++) {
+//     numsMap[nums[i]] = i;
+//   }
+
+//   for (let i = 0; i < nums.length; i++) {
+//     const diff = target - nums[i];
+
+//     // second condition is for: we can't use same element twice
+//     if (diff in numsMap && i !== numsMap[diff])
+//       return [i, numsMap[diff]];
+//   }
+// };
+
+// With better optimizations:
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
 var twoSum = function (nums, target) {
   const numsMap = {};
 
   for (let i = 0; i < nums.length; i++) {
-    numsMap[nums[i]] = i;
-  }
-
-  for (let i = 0; i < nums.length; i++) {
     const diff = target - nums[i];
 
-    // second condition is for: we can't use same element twice
-    if (diff in numsMap && i !== numsMap[diff])
-      return [i, numsMap[diff]];
+    if (diff in numsMap) {
+      return [numsMap[diff], i];
+    }
+
+    numsMap[nums[i]] = i;
   }
 };
