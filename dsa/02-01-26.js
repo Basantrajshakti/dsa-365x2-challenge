43e86bd831ae1345c91622a75a1f188cc83581cd
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-02 20:18:24 +0530

    26. Remove Duplicates from Sorted Array optimized

--- dsa/02-01-26.js
+++ dsa/02-01-26.js
@@ -1,15 +1,30 @@
 // Arrays
 
 // 26. Remove Duplicates from Sorted Array
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var removeDuplicates = function (nums) {
+//   const numsUnique = Array.from(new Set(nums));
+
+//   for (let i = 0; i < numsUnique.length; i++) {
+//     nums[i] = numsUnique[i];
+//   }
+//   return numsUnique.length;
+// };
+
+// With better optimizations:
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-  const numsUnique = Array.from(new Set(nums));
-
-  for (let i = 0; i < numsUnique.length; i++) {
-    nums[i] = numsUnique[i];
+  let i = 1;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[i - 1] !== nums[j]) {
+      nums[i++] = nums[j];
+    }
   }
-  return numsUnique.length;
+  return i;
 };
