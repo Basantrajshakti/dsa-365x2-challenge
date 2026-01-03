5287dde7158a58dee67a8f0b82e023c541870140
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-03 22:19:05 +0530

    Code cleanings

--- dsa/03-01-26.js
+++ dsa/03-01-26.js
@@ -6,15 +6,12 @@
  * @return {number[]}
  */
 var plusOne = function (digits) {
-  let carryForward = 0;
 
   for (let i = digits.length - 1; i >= 0; i--) {
     if (digits[i] === 9) {
-      carryForward = 1;
       digits[i] = 0;
     } else {
       digits[i] += 1;
-      carryForward = 0;
       return digits;
     }
   }
