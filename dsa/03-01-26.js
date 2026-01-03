d09fc144ae755ac1d177e803aae343b0cb6f68b8
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-03 20:10:48 +0530

    35. Search Insert Position

--- dsa/03-01-26.js
+++ dsa/03-01-26.js
@@ -21,3 +21,27 @@ var plusOne = function (digits) {
 
   return [1, ...digits];
 };
+
+
+// 35. Search Insert Position
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  let low = 0;
+  let high = nums.length - 1;
+
+  while (1) {
+    if (low > high) return low;
+    let mid = parseInt((low + high) / 2);
+
+    if (nums[mid] === target) return mid;
+    else if (nums[mid] > target) {
+      high = mid - 1;
+    } else if (nums[mid] < target) {
+      low = mid + 1;
+    }
+  }
+};
