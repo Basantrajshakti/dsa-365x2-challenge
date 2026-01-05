ce13c798716b16e45862a5b867bc67e322a1d5d1
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-05 22:03:16 +0530

    88. Merge Sorted Array optimized

--- dsa/05-01-26.js
+++ dsa/05-01-26.js
@@ -44,17 +44,30 @@ var getRow = function (rowIndex) {
 
 
 // 88. Merge Sorted Array
-/**
- * @param {number[]} nums1
- * @param {number} m
- * @param {number[]} nums2
- * @param {number} n
- * @return {void} Do not return anything, modify nums1 in-place instead.
- */
-var merge = function (nums1, m, nums2, n) {
-  for (let i = m + n, j = n; j > 0; i--, j--) {
-    nums1[i - 1] = nums2[j - 1];
-  }
+// /**
+//  * @param {number[]} nums1
+//  * @param {number} m
+//  * @param {number[]} nums2
+//  * @param {number} n
+//  * @return {void} Do not return anything, modify nums1 in-place instead.
+//  */
+// var merge = function (nums1, m, nums2, n) {
+//   for (let i = m + n, j = n; j > 0; i--, j--) {
+//     nums1[i - 1] = nums2[j - 1];
+//   }
 
-  return nums1.sort((a, b) => a - b);
-};
+//   return nums1.sort((a, b) => a - b);
+// };
+
+// With optimizations:
+let i = m - 1;
+let j = n - 1;
+let k = m + n - 1;
+
+while (j >= 0) {
+  if (i >= 0 && nums1[i] > nums2[j]) {
+    nums1[k--] = nums1[i--];
+  } else {
+    nums1[k--] = nums2[j--];
+  }
+}
