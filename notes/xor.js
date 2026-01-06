b5175b38081a8f4d8b69d853b0fa7bb462e6fcef
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-06 20:58:36 +0530

    XOR notes added

---
+++ notes/xor.js
@@ -0,0 +1,4 @@
+// XOR(eXclusive OR) is a binary operation that compares two bits and outputs a result based on the following rules:
+
+// Same bits: 0 ^ 0 = 0, 1 ^ 1 = 0
+// Different bits: 0 ^ 1 = 1, 1 ^ 0 = 1
