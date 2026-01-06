e2eff8a4c1c409531a5496ca81b5d3c15755fa9a
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-06 20:59:08 +0530

    136. Single Number

--- dsa/06-01-26.js
+++ dsa/06-01-26.js
@@ -18,3 +18,18 @@ var maxProfit = function (prices) {
 
   return maxProfit;
 };
+
+// 136. Single Number
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+  let num = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    num ^= nums[i];
+  }
+
+  return num;
+};
