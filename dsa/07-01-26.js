4417ea080d313c767f9e26bbb8bd67743685188f
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-07 17:54:50 +0530

    169. Majority Element optimized

--- dsa/07-01-26.js
+++ dsa/07-01-26.js
@@ -1,31 +1,52 @@
 // Arrays
 
 // 169. Majority Element
+// /**
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// var majorityElement = function (nums) {
+//   let map = {};
+
+//   for (let i = 0; i < nums.length; i++) {
+//     const num = nums[i];
+//     if (num in map) {
+//       map[num] += 1;
+//     } else {
+//       map[num] = 1;
+//     }
+//   }
+
+//   let majority = 0;
+//   let maxCount = 0;
+
+//   for (const key in map) {
+//     if (map[key] > maxCount) {
+//       majority = key;
+//       maxCount = map[key];
+//     }
+//   }
+
+//   return +majority;
+// };
+
+// With optimizations
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var majorityElement = function (nums) {
-  let map = {};
+  let count = 0, maj = 0;
 
   for (let i = 0; i < nums.length; i++) {
-    const num = nums[i];
-    if (num in map) {
-      map[num] += 1;
+    if (count === 0) {
+      maj = nums[i];
+    }
+    if (maj === nums[i]) {
+      count++;
     } else {
-      map[num] = 1;
+      count--;
     }
   }
-
-  let majority = 0;
-  let maxCount = 0;
-
-  for (const key in map) {
-    if (map[key] > maxCount) {
-      majority = key;
-      maxCount = map[key];
-    }
-  }
-
-  return +majority;
+  return maj;
 };
