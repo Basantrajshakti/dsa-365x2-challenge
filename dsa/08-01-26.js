0a7bb26b4918775d8aad26d451d235d6b46f48a4
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-08 20:35:51 +0530

    228. Summary Ranges

--- dsa/08-01-26.js
+++ dsa/08-01-26.js
@@ -1,25 +1,25 @@
 // Arrays
 
-// 219. Contains Duplicate II
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {boolean}
- */
-var containsNearbyDuplicate = function (nums, k) {
-  const map = {};
+// // 219. Contains Duplicate II
+// /**
+//  * @param {number[]} nums
+//  * @param {number} k
+//  * @return {boolean}
+//  */
+// var containsNearbyDuplicate = function (nums, k) {
+//   const map = {};
 
-  for (let i = 0; i < nums.length; i++) {
-    const num = nums[i];
-    if (num in map && Math.abs(map[num] - i) <= k) {
-      return true;
-    }
+//   for (let i = 0; i < nums.length; i++) {
+//     const num = nums[i];
+//     if (num in map && Math.abs(map[num] - i) <= k) {
+//       return true;
+//     }
 
-    map[num] = i;
-  }
+//     map[num] = i;
+//   }
 
-  return false;
-};
+//   return false;
+// };
 
 // With optimizations: Copied
 /**
@@ -41,4 +41,31 @@ var containsNearbyDuplicate = function (nums, k) {
   }
 
   return false;
+};
+
+
+// 228. Summary Ranges
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function (nums) {
+  if (nums.length === 0) return [];
+
+  let summary = [];
+  let flag = nums[0];
+  let ctr = flag;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (flag + 1 === nums[i]) {
+      flag = nums[i];
+    } else {
+      summary.push(ctr === flag ? `${ctr}` : `${ctr}->${flag}`)
+      ctr = flag = nums[i];
+    }
+  }
+
+  summary.push(ctr === flag ? `${ctr}` : `${ctr}->${flag}`)
+
+  return summary;
 };
