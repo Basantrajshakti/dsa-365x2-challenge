bfb79390109216cb88b0d1bcacccd06e6ea8515c
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-09 21:51:30 +0530

    283. Move Zeroes optimized

--- dsa/09-01-26.js
+++ dsa/09-01-26.js
@@ -1,21 +1,41 @@
 // Arrays
 
 // 283. Move Zeroes
+// /**
+//  * @param {number[]} nums
+//  * @return {void} Do not return anything, modify nums in-place instead.
+//  */
+// var moveZeroes = function (nums) {
+//   if (nums.length === 0) return num;
+
+//   for (let i = 0, j = 0; i < nums.length; i++) {
+//     if (nums[i] !== 0 && nums[j] === 0) {
+//       nums[j++] = nums[i];
+//       nums[i] = 0;
+//     }
+
+//     if (nums[j] !== 0) j++;
+//   }
+
+//   return nums;
+// };
+
+
+// With optimizations
 /**
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function (nums) {
-  if (nums.length === 0) return num;
+  if (nums.length === 0) return nums;
 
-  for (let i = 0, j = 0; i < nums.length; i++) {
-    if (nums[i] !== 0 && nums[j] === 0) {
-      nums[j++] = nums[i];
-      nums[i] = 0;
+  let ctr = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      const temp = nums[i];
+      nums[i] = nums[ctr];
+      nums[ctr++] = temp;
     }
-
-    if (nums[j] !== 0) j++;
   }
-
-  return nums;
 };
