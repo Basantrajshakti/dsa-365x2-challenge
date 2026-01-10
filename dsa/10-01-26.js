1b3ad5d3d522bae04aedfbce6c1f47aea4c4f308
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-10 19:56:53 +0530

    349. Intersection of Two Arrays II

--- dsa/10-01-26.js
+++ dsa/10-01-26.js
@@ -19,3 +19,31 @@ var intersection = function (nums1, nums2) {
   return nums;
 };
 
+
+// 350. Intersection of Two Arrays II
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function (nums1, nums2) {
+  const nums1Map = {};
+  const res = [];
+
+  for (let i = 0; i < nums1.length; i++) {
+    if (nums1[i] in nums1Map) {
+      nums1Map[nums1[i]] += 1;
+    } else {
+      nums1Map[nums1[i]] = 1;
+    }
+  }
+
+  for (let i = 0; i < nums2.length; i++) {
+    if (nums1Map[nums2[i]] > 0) {
+      res.push(nums2[i]);
+      nums1Map[nums2[i]] -= 1;
+    }
+  }
+
+  return res;
+};
