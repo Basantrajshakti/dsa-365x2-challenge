b0546d7560ca0a1f84793ab8fe0d825771f02fb6
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-11 20:44:31 +0530

    13. Roman to Integer optimized

--- dsa/11-01-26.js
+++ dsa/11-01-26.js
@@ -5,33 +5,66 @@
  * @param {string} s
  * @return {number}
  */
-const romanMap = {
-  "I": 1,
-  "V": 5,
-  "X": 10,
-  "L": 50,
-  "C": 100,
-  "D": 500,
-  "M": 1000,
-  "IV": 4,
-  "IX": 9,
-  "XL": 40,
-  "XC": 90,
-  "CD": 400,
-  "CM": 900,
-  "II": 2,
-};
+// const romanMap = {
+//   "I": 1,
+//   "V": 5,
+//   "X": 10,
+//   "L": 50,
+//   "C": 100,
+//   "D": 500,
+//   "M": 1000,
+//   "IV": 4,
+//   "IX": 9,
+//   "XL": 40,
+//   "XC": 90,
+//   "CD": 400,
+//   "CM": 900,
+//   "II": 2,
+// };
 
+// var romanToInt = function (s) {
+//   let sum = 0;
+
+//   for (let i = 0; i < s.length; i++) {
+//     if ((s[i] + s[i + 1]) in romanMap) {
+//       sum += romanMap[s[i] + s[i + 1]];
+//       i++; // i+1 is already captured
+//     } else {
+//       sum += romanMap[s[i]];
+//     }
+//   }
+//   return sum;
+// };
+
+// With optimizations
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var romanToInt = function (s) {
-  let sum = 0;
+  const map = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+  };
 
-  for (let i = 0; i < s.length; i++) {
-    if ((s[i] + s[i + 1]) in romanMap) {
-      sum += romanMap[s[i] + s[i + 1]];
-      i++; // i+1 is already captured
+  let total = 0;
+  let prev = 0;
+
+  for (let i = s.length - 1; i >= 0; i--) {
+    let curr = map[s[i]];
+
+    if (curr < prev) {
+      total -= curr;
     } else {
-      sum += romanMap[s[i]];
+      total += curr;
     }
+    prev = curr;
   }
-  return sum;
+
+  return total;
 };
