9255ff0a1de7ba2d1f597dd8f407e4eec952bb35
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-12 20:03:14 +0530

    28. Find the Index of the First Occurrence in a String custom implementation

--- dsa/12-01-26.js
+++ dsa/12-01-26.js
@@ -1,12 +1,30 @@
 // Strings
 
 // 28. Find the Index of the First Occurrence in a String
+// /**
+//  * @param {string} haystack
+//  * @param {string} needle
+//  * @return {number}
+//  */
+// var strStr = function (haystack, needle) {
+//   return haystack.indexOf(needle);
+// };
+
+// Custom implementation
 /**
  * @param {string} haystack
  * @param {string} needle
  * @return {number}
  */
 var strStr = function (haystack, needle) {
-  return haystack.indexOf(needle);
-};
+  if (needle.length > haystack.length) return -1;
 
+  const needleLen = needle.length;
+  const loopLen = haystack.length - needleLen + 1;
+
+  for (let i = 0; i < loopLen; i++) {
+    const tempNeedle = haystack.substring(i, needleLen + i);
+    if (tempNeedle === needle) return i;
+  }
+  return -1;
+};
