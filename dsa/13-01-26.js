10ebf6294ff2c9d11abf9e1e07545c1aa431d367
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-13 18:45:25 +0530

    67. Add Binary optimized

--- dsa/13-01-26.js
+++ dsa/13-01-26.js
@@ -1,36 +1,56 @@
 // Strings
 
 // 67. Add Binary
+// /**
+//  * @param {string} a
+//  * @param {string} b
+//  * @return {string}
+//  */
+
+// Logic:
+// 1+1=0 with carry 1
+// 1+0=1 with carry 0
+// 0+1=1 with carry 0
+// 0+0=0 with carry 0
+// Imp:1+1=1 with carry 1 if previous carry was 1.
+
+
+// var addBinary = function (a, b) {
+//   let bin = '';
+//   let carry = 0;
+
+//   for (let i = a.length - 1, j = b.length - 1; i >= 0 || j >= 0; j--, i--) {
+//     const c1 = +a[i] || 0;
+//     const c2 = +b[j] || 0;
+//     const sum = c1 + c2 + carry;
+
+//     if (sum === 3) {
+//       carry = 1;
+//       bin = '1' + bin;
+//     } else if (sum === 2) {
+//       carry = 1;
+//       bin = '0' + bin;
+//     }
+//     else {
+//       carry = 0;
+//       bin = sum + bin;
+//     }
+//   }
+
+//   if (carry == 1) {
+//     bin = '1' + bin;
+//   }
+
+//   return bin;
+// };
+
+
+// With optimizations
 /**
  * @param {string} a
  * @param {string} b
  * @return {string}
  */
 var addBinary = function (a, b) {
-  let bin = '';
-  let carry = 0;
-
-  for (let i = a.length - 1, j = b.length - 1; i >= 0 || j >= 0; j--, i--) {
-    const c1 = +a[i] || 0;
-    const c2 = +b[j] || 0;
-    const sum = c1 + c2 + carry;
-
-    if (sum === 3) {
-      carry = 1;
-      bin = '1' + bin;
-    } else if (sum === 2) {
-      carry = 1;
-      bin = '0' + bin;
-    }
-    else {
-      carry = 0;
-      bin = sum + bin;
-    }
-  }
-
-  if (carry == 1) {
-    bin = '1' + bin;
-  }
-
-  return bin;
+  return (BigInt("0b" + a) + BigInt("0b" + b)).toString(2);
 };
