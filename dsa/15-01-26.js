5d1a105b5fd13aa25c6815111d9836442985fccd
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-15 20:19:33 +0530

    205. Isomorphic Strings optimized

--- dsa/15-01-26.js
+++ dsa/15-01-26.js
@@ -1,6 +1,28 @@
 // Strings
 
 // 205. Isomorphic Strings
+// /**
+//  * @param {string} s
+//  * @param {string} t
+//  * @return {boolean}
+//  */
+// var isIsomorphic = function (s, t) {
+//   if (s.length !== t.length) return false;
+
+//   const sIndMap = {};
+//   const tIndMap = {};
+
+//   for (let i = 0; i < s.length; i++) {
+//     if (!(s[i] in sIndMap)) sIndMap[s[i]] = i;
+//     if (!(t[i] in tIndMap)) tIndMap[t[i]] = i;
+
+//     if (sIndMap[s[i]] != tIndMap[t[i]]) return false;
+//   }
+
+//   return true;
+// };
+
+// With optimizations
 /**
  * @param {string} s
  * @param {string} t
@@ -9,14 +31,17 @@
 var isIsomorphic = function (s, t) {
   if (s.length !== t.length) return false;
 
-  const sIndMap = {};
-  const tIndMap = {};
+  const mapST = new Int16Array(128);
+  const mapTS = new Int16Array(128);
 
   for (let i = 0; i < s.length; i++) {
-    if (!(s[i] in sIndMap)) sIndMap[s[i]] = i;
-    if (!(t[i] in tIndMap)) tIndMap[t[i]] = i;
+    const a = s.charCodeAt(i);
+    const b = t.charCodeAt(i);
 
-    if (sIndMap[s[i]] != tIndMap[t[i]]) return false;
+    if (mapST[a] == 0 && mapTS[b] == 0) {
+      mapST[a] = b + 1;
+      mapTS[b] = a + 1;
+    } else if (mapST[a] != b + 1 || mapTS[b] != a + 1) return false;
   }
 
   return true;
