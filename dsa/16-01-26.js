ddca0df98a9a8781b34286b721440e9707586fd7
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-16 20:38:36 +0530

    345. Reverse Vowels of a String

--- dsa/16-01-26.js
+++ dsa/16-01-26.js
@@ -12,3 +12,26 @@ var reverseString = function (s) {
     s[j] = temp;
   }
 };
+
+
+// 345. Reverse Vowels of a String
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const v = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
+
+var reverseVowels = function (s) {
+  const rev = s.split('');
+
+  for (let i = 0, j = s.length - 1; i < j; i++, j--) {
+    while (i < j && !v.has(rev[i])) i++;
+    while (i < j && !v.has(rev[j])) j--;
+
+    const temp = rev[i];
+    rev[i] = rev[j];
+    rev[j] = temp;
+  }
+
+  return rev.join('');
+};
