03fe17420d6694f9597da25983c7968ecf5b574b
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-18 17:50:38 +0530

    387. First Unique Character in a String optimized

--- dsa/18-01-26.js
+++ dsa/18-01-26.js
@@ -1,6 +1,22 @@
 // Strings
 
 // 387. First Unique Character in a String
+// /**
+//  * @param {string} s
+//  * @return {number}
+//  */
+// var firstUniqChar = function (s) {
+//   if (s.length === 1) return 0;
+
+//   for (let i = 0; i < s.length; i++) {
+//     if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) return i;
+//   }
+
+//   return -1;
+// };
+
+
+// With optimizations
 /**
  * @param {string} s
  * @return {number}
@@ -8,8 +24,14 @@
 var firstUniqChar = function (s) {
   if (s.length === 1) return 0;
 
+  const f = new Uint32Array(26);
+
   for (let i = 0; i < s.length; i++) {
-    if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) return i;
+    f[s.charCodeAt(i) - 97]++;
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    if (f[s.charCodeAt(i) - 97] === 1) return i;
   }
 
   return -1;
