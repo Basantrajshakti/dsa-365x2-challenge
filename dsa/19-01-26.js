36aaff7dbb711e3b38e8a9b494a7e53ef913b358
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-19 16:49:57 +0530

    392. Is Subsequence

---
+++ dsa/19-01-26.js
@@ -0,0 +1,16 @@
+// Strings
+
+// 392. Is Subsequence
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function (s, t) {
+  let j = 0;
+  for (let i = 0; i < t.length; i++) {
+    if (s[j] === t[i]) j++;
+  }
+
+  return j === s.length;
+};
