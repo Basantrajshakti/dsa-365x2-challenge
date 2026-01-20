df382c36827d9049fb25b0d6f888307391255f62
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-20 17:59:49 +0530

    409. Longest Palindrome optimized

--- dsa/20-01-26.js
+++ dsa/20-01-26.js
@@ -1,30 +1,59 @@
 // Strings
 
 // 409. Longest Palindrome
+// /**
+//  * @param {string} s
+//  * @return {number}
+//  */
+// var longestPalindrome = function (s) {
+//   if (s.length === 1) return 1;
+
+//   const f = new Uint16Array(58);
+
+//   for (let i = 0; i < s.length; i++) {
+//     f[s.charCodeAt([i]) - 65]++;
+//   }
+
+//   let sum = 0;
+
+//   for (let i = 0; i < f.length; i++) {
+//     if (f[i] === 0) continue;
+
+//     else if (f[i] % 2 === 0) {
+//       sum += f[i];
+//     } else {
+//       sum += f[i] - 1;
+//     }
+//   }
+
+//   return sum < s.length ? sum + 1 : sum;
+// };
+
+// With optimizations
 /**
  * @param {string} s
  * @return {number}
  */
 var longestPalindrome = function (s) {
-  if (s.length === 1) return 1;
-
-  const f = new Uint16Array(58);
+  const m = new Map();
 
   for (let i = 0; i < s.length; i++) {
-    f[s.charCodeAt([i]) - 65]++;
+    m.set(s[i], (m.get(s[i]) || 0) + 1);
   }
 
   let sum = 0;
+  let hasOdd = false;
 
-  for (let i = 0; i < f.length; i++) {
-    if (f[i] === 0) continue;
-
-    else if (f[i] % 2 === 0) {
-      sum += f[i];
+  for (let v of m.values()) {
+    if (v % 2 === 0) {
+      sum += v;
     } else {
-      sum += f[i] - 1;
+      sum += v - 1;
+      hasOdd = true;
     }
   }
 
-  return sum < s.length ? sum + 1 : sum;
+  if (hasOdd) sum += 1;
+
+  return sum;
 };
