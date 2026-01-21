6195b940b272a98faecc9edbf591aeb7118d1b30
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-21 22:03:46 +0530

    69. Sqrt(x) optimized

--- dsa/21-01-26.js
+++ dsa/21-01-26.js
@@ -54,3 +54,23 @@ var mySqrt = function (x) {
 
   return ans;
 };
+
+// With optimizations
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+  let l = 0, r = x, ans = 0;
+
+  while (l <= r) {
+    let m = Math.floor((l + r) / 2);
+    if (m * m <= x) {
+      ans = m;
+      l = m + 1;
+    } else {
+      r = m - 1;
+    }
+  }
+  return ans;
+};
