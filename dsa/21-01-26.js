6bd4dff5fb722e70f946219336de8583dd314b76
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-21 22:01:38 +0530

    69. Sqrt(x)

--- dsa/21-01-26.js
+++ dsa/21-01-26.js
@@ -18,3 +18,39 @@ var isPalindrome = function (x) {
 
   return +xStr === n;
 };
+
+
+// 69. Sqrt(x)
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+  let start = 0, end = x, mid, ans;
+
+  while (start <= end) {
+
+    mid = parseInt(start + (end - start) / 2);
+
+    if (mid * mid == x) {
+      ans = mid;
+      break;
+    }
+
+    // Increment start if integral part lies on right side of the mid
+    if (mid * mid < x) {
+      // First start value should be added to answer
+      ans = start;
+      // Then start should be changed
+      start = mid + 1;
+    }
+
+    // Decrement end if integral part lies on the left side of the mid
+    else {
+      end = mid - 1;
+      ans = end;
+    }
+  }
+
+  return ans;
+};
