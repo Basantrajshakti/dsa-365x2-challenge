5f8c5b3ad6bb535b68d0fd8a6b1e4a5e4b33f6d1
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-22 19:15:30 +0530

    202. Happy Number optimized

--- dsa/22-01-26.js
+++ dsa/22-01-26.js
@@ -38,3 +38,30 @@ var isHappy = function (n) {
 
   return true;
 };
+
+// With optimizations
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function (n) {
+  let sum = 0;
+  let digit = n;
+  let set = new Set();
+
+  while (sum !== 1) {
+    sum = 0;
+    while (digit > 0) {
+      const num = digit % 10;
+      sum += num * num;
+      digit = Math.floor(digit / 10);
+    }
+
+    if (set.has(sum)) return false;
+
+    set.add(sum);
+    digit = sum;
+  }
+
+  return true;
+};
