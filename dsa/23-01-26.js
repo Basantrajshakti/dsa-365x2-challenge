780ad70b0c6caef86471e4ab52be46160a841b8e
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-23 21:11:29 +0530

    263. Ugly Number

--- dsa/23-01-26.js
+++ dsa/23-01-26.js
@@ -34,3 +34,19 @@ var addDigits = function (num) {
 
   return 1 + ((num - 1) % 9);
 };
+
+
+// 263. Ugly Number
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isUgly = function (n) {
+  if (n <= 0) return false;
+
+  while (n % 2 === 0) n /= 2;
+  while (n % 3 === 0) n /= 3;
+  while (n % 5 === 0) n /= 5;
+
+  return n === 1;
+};
