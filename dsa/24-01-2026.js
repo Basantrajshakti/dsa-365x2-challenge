72fc3e873b4c73643ca01d5027f3ec1c32d431b5
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-24 22:13:27 +0530

    268. Missing Number

---
+++ dsa/24-01-2026.js
@@ -0,0 +1,15 @@
+// Maths
+
+// 268. Missing Number
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+  const len = nums.length;
+  const s = new Set(nums);
+
+  for (let i = 0; i <= len; i++)
+    if (!s.has(i))
+      return i;
+};
