1398952803e5e0a1526004bb9d34a19f7b9563bf
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-24 22:45:21 +0530

    292. Nim Game

--- dsa/24-01-2026.js
+++ dsa/24-01-2026.js
@@ -29,3 +29,13 @@ var missingNumber = function (nums) {
 
   return sum;
 };
+
+
+// 292. Nim Game
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var canWinNim = function (n) {
+  return n % 4 !== 0;
+};
