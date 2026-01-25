a5d755e4988f5a46e3c81f72109178fd3fa96447
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-25 18:48:42 +0530

    326. Power of Three

---
+++ dsa/25-04-26.js
@@ -0,0 +1,13 @@
+// Maths
+
+// 326. Power of Three
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function (n) {
+  while (n >= 3) {
+    n = n / 3;
+  }
+  return n === 1
+};
