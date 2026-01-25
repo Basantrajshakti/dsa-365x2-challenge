47c5be76a80cbdbf71e147008d661ae38c7c9716
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-25 18:49:30 +0530

    326. Power of Three optimized

--- dsa/25-04-26.js
+++ dsa/25-04-26.js
@@ -11,3 +11,12 @@ var isPowerOfThree = function (n) {
   }
   return n === 1
 };
+
+// With optimizations
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function (n) {
+  return Number.isInteger(Math.log10(n) / Math.log10(3));
+};
