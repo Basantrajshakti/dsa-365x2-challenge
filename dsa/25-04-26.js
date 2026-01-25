da019f3790cc091c5b36049e08083016df117386
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-25 18:50:48 +0530

    342. Power of Four optimized

--- dsa/25-04-26.js
+++ dsa/25-04-26.js
@@ -33,3 +33,12 @@ var isPowerOfFour = function (n) {
   }
   return n === 1
 };
+
+// With optimizations
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function (n) {
+  return Number.isInteger(Math.log10(n) / Math.log10(4));
+};
