8a92077d619e71387942b0cb0a85e158871a15c6
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-26 21:27:25 +0530

    367. Valid Perfect Square optimized

--- dsa/26-01-26.js
+++ dsa/26-01-26.js
@@ -19,3 +19,13 @@ var isPerfectSquare = function (num) {
 
   return false;
 };
+
+
+// Optimized: not allowed
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function (num) {
+  return Number.isInteger(Math.pow(num, 0.5));
+};
