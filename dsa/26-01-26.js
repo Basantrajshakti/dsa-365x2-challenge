f88bdfbfc1579b1c084537c6582d42af6b52a7bd
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-26 21:39:09 +0530

    415. Add Strings

--- dsa/26-01-26.js
+++ dsa/26-01-26.js
@@ -29,3 +29,31 @@ var isPerfectSquare = function (num) {
 var isPerfectSquare = function (num) {
   return Number.isInteger(Math.pow(num, 0.5));
 };
+
+
+// 415. Add Strings
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function (num1, num2) {
+  let sum = '';
+  let carry = 0;
+
+  for (let i = num1.length - 1, j = num2.length - 1; i >= 0 || j >= 0; i--, j--) {
+    const lastDigSum = (+num1[i] || 0) + (+num2[j] || 0) + carry + '';
+
+    if (lastDigSum.length > 1) {
+      sum = lastDigSum[lastDigSum.length - 1] + sum;
+      carry = +lastDigSum[0];
+    } else {
+      sum = lastDigSum + sum;
+      carry = 0;
+    }
+  }
+
+  if (carry) return carry + sum;
+
+  return sum;
+};
