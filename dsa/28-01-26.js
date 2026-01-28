1d7fbd914c3081fe1ba982e32fd51d9d8c6aec87
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-28 14:37:45 +0530

    507. Perfect Number

--- dsa/28-01-26.js
+++ dsa/28-01-26.js
@@ -24,3 +24,19 @@ var convertToBase7 = function (num) {
   if (isNeg) return '-' + base;
   return base;
 };
+
+
+// 507. Perfect Number
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var checkPerfectNumber = function (num) {
+  let sum = 0;
+
+  for (let i = 1; i <= num / 2; i++) {
+    if (num % i === 0) sum += i;
+  }
+
+  return sum === num;
+};
