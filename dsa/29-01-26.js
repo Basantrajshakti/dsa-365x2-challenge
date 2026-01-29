b510000ababca945af377749ed5f2a2f01b904d2
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-29 20:52:06 +0530

    598. Range Addition II

--- dsa/29-01-26.js
+++ dsa/29-01-26.js
@@ -28,3 +28,28 @@ var fib = function (n) {
 
   return fib(n - 1) + fib(n - 2);
 };
+
+
+
+// 598. Range Addition II
+/**
+ * @param {number} m
+ * @param {number} n
+ * @param {number[][]} ops
+ * @return {number}
+ */
+var maxCount = function (m, n, ops) {
+  if (ops.length === 0) {
+    return m * n;
+  }
+
+  let minRow = m;
+  let minCol = n;
+
+  for (let i = 0; i < ops.length; i++) {
+    minRow = Math.min(minRow, ops[i][0]);
+    minCol = Math.min(minCol, ops[i][1]);
+  }
+
+  return minRow * minCol;
+};
