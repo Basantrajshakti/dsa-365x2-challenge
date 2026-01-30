8dcc5228c7a8e457efb8a08470bc7c06b0ee2847
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-30 19:52:41 +0530

    628. Maximum Product of Three Numbers optimized further

--- dsa/30-01-26.js
+++ dsa/30-01-26.js
@@ -38,3 +38,43 @@ var maximumProduct = function (nums) {
   if (p1 > p2) return p1;
   return p2;
 };
+
+
+// Optimized further
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumProduct = function (nums) {
+  let max1 = -Infinity;
+  let max2 = -Infinity;
+  let max3 = -Infinity;
+  let min1 = Infinity;
+  let min2 = Infinity;
+
+  for (num of nums) {
+    if (num > max1) {
+      max3 = max2;
+      max2 = max1;
+      max1 = num;
+    } else if (num > max2) {
+      max3 = max2;
+      max2 = num;
+    } else if (num > max3) {
+      max3 = num;
+    }
+
+    if (num < min1) {
+      min2 = min1;
+      min1 = num;
+    } else if (num < min2) {
+      min2 = num;
+    }
+  }
+
+  const p1 = max1 * max2 * max3;
+  const p2 = min1 * min2 * max1;
+
+  if (p1 > p2) return p1;
+  return p2;
+};
