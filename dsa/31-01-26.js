354b9c79dd5634fa355c2c7ee19e9d6febf09add
Basantraj.shakti <basantrajshakti@gmail.com>
2026-01-31 23:03:12 +0530

    594. Longest Harmonious Subsequence optimized

--- dsa/31-01-26.js
+++ dsa/31-01-26.js
@@ -8,7 +8,7 @@
 var findLHS = function (nums) {
   const map = new Map();
 
-  for (num of nums) {
+  for (const num of nums) {
     map.set(num, (map.get(num) || 0) + 1)
   }
 
@@ -20,6 +20,28 @@ var findLHS = function (nums) {
 
     if (map.get(key + 1) && max < (value + map.get(key + 1))) {
       max = value + map.get(key + 1);
+    }
+  }
+
+  return max;
+};
+
+// With optimizations 
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLHS = function (nums) {
+  const map = new Map();
+
+  for (const num of nums) {
+    map.set(num, (map.get(num) || 0) + 1)
+  }
+
+  let max = 0;
+  for (const [key, value] of map) {
+    if (map.has(key + 1)) {
+      max = Math.max(max, value + map.get(key + 1))
     }
   }
 
