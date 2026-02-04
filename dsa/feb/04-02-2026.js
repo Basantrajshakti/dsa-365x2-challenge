ee36d267316d370c5b4b8ccc1d48bb64f7c06d6d
Basantraj.shakti <basantrajshakti@gmail.com>
2026-02-04 21:38:37 +0530

    3090. Maximum Length Substring With Two Occurrences optimized

--- dsa/feb/04-02-2026.js
+++ dsa/feb/04-02-2026.js
@@ -26,3 +26,23 @@ var maximumLengthSubstring = function (s) {
 
   return max;
 };
+
+// With optimizations
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maximumLengthSubstring = function (s) {
+  let left = 0, freq = new Map(), longest = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    freq.set(s[right], (freq.get(s[right]) || 0) + 1);
+
+    while (freq.get(s[right]) > 2) {
+      freq.set(s[left], freq.get(s[left]) - 1);
+      left++;
+    }
+    longest = Math.max(longest, right - left + 1);
+  }
+  return longest;
+};
