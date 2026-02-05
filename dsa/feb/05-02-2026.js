d4f0426e8f097778b81a8f5448d934d6e7cdd898
Basantraj.shakti <basantrajshakti@gmail.com>
2026-02-05 19:15:36 +0530

    3206. Alternating Groups I cleaned

--- dsa/feb/05-02-2026.js
+++ dsa/feb/05-02-2026.js
@@ -17,3 +17,22 @@ var numberOfAlternatingGroups = function (colors) {
 
   return ctr;
 };
+
+// Cleaned up
+/**
+ * @param {number[]} colors
+ * @return {number}
+ */
+var numberOfAlternatingGroups = function (colors) {
+  let ctr = 0;
+  const len = colors.length;
+
+  for (let i = 0; i < len; i++) {
+    const left = (i - 1 + len) % len;
+    const right = (i + 1 + len) % len;
+
+    if (colors[i] !== colors[right] && colors[i] !== colors[left]) ctr++;
+  }
+
+  return ctr;
+};
