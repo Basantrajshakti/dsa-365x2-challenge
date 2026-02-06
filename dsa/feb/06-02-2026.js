7c915b5553209c862062309c1f144733c08aa6b8
Basantraj.shakti <basantrajshakti@gmail.com>
2026-02-06 23:42:29 +0530

    3411. Maximum Subarray With Equal Products

--- dsa/feb/06-02-2026.js
+++ dsa/feb/06-02-2026.js
@@ -55,3 +55,41 @@ var minimumSumSubarray = function (nums, l, r) {
   if (minSum == Infinity) return -1;
   return minSum;
 };
+
+
+// 3411. Maximum Subarray With Equal Products
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxLength = function (nums) {
+  const n = nums.length;
+  let maxLength = 0;
+
+  for (let start = 0; start < n; start++) {
+    let currentGcd = nums[start];
+    let currentLcm = nums[start];
+    let currentProduct = nums[start];
+
+    for (let end = start; end < n; end++) {
+      if (end > start) {
+        currentGcd = gcd(currentGcd, nums[end]);
+        currentLcm = (currentLcm * nums[end]) / gcd(currentLcm, nums[end]);
+        currentProduct *= nums[end];
+      }
+      if (currentProduct === currentLcm * currentGcd) {
+        maxLength = Math.max(maxLength, end - start + 1);
+      }
+    }
+  }
+  return maxLength;
+};
+
+function gcd(a, b) {
+  while (b !== 0) {
+    const temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
+}
