382e103be25535fafe70abe2a6deed050c3fb206
Basantraj.shakti <basantrajshakti@gmail.com>
2026-02-06 23:37:30 +0530

    3364. Minimum Positive Sum Subarray cleaned

--- dsa/feb/06-02-2026.js
+++ dsa/feb/06-02-2026.js
@@ -27,3 +27,31 @@ var minimumSumSubarray = function (nums, l, r) {
 
   return minSum;
 };
+
+// Cleaned up code
+/**
+ * @param {number[]} nums
+ * @param {number} l
+ * @param {number} r
+ * @return {number}
+ */
+var minimumSumSubarray = function (nums, l, r) {
+  let minSum = Infinity;
+  const len = nums.length;
+
+  for (let i = 0; i < len; i++) {
+    let sum = 0;
+
+    for (let j = i; j < len; j++) {
+      sum += nums[j];
+
+      if (j - i + 1 >= l && j - i + 1 <= r && sum > 0) {
+        minSum = Math.min(minSum, sum);
+      }
+      if (j - i + 1 > r) break;
+    }
+  }
+
+  if (minSum == Infinity) return -1;
+  return minSum;
+};
