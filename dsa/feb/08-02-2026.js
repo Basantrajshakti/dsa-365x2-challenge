9d853415c823f65e07473cf16cffabd316a25c5f
Basantraj.shakti <basantrajshakti@gmail.com>
2026-02-08 19:43:31 +0530

    599. Minimum Index Sum of Two Lists

---
+++ dsa/feb/08-02-2026.js
@@ -0,0 +1,33 @@
+// Hash map
+
+// 599. Minimum Index Sum of Two Lists
+/**
+ * @param {string[]} list1
+ * @param {string[]} list2
+ * @return {string[]}
+ */
+var findRestaurant = function (list1, list2) {
+  const map = {};
+  let min = Infinity;
+  let arr = [];
+
+  for (let i = 0; i < list1.length; i++) {
+    map[list1[i]] = i;
+  }
+
+  for (let i = 0; i < list2.length; i++) {
+    const l = list2[i];
+    if (l in map) {
+      const sum = i + map[l];
+
+      if (sum < min) {
+        arr = [l];
+        min = sum;
+      } else if (sum === min) {
+        arr.push(l);
+      }
+    }
+  }
+
+  return arr;
+};
