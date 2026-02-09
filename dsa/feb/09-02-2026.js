9400c149e3aa39050a1bc6830e5f2a7d464d7460
Basantraj.shakti <basantrajshakti@gmail.com>
2026-02-09 22:01:55 +0530

    706. Design HashMap

--- dsa/feb/09-02-2026.js
+++ dsa/feb/09-02-2026.js
@@ -28,3 +28,35 @@ MyHashSet.prototype.remove = function (key) {
 MyHashSet.prototype.contains = function (key) {
   return !!this.data[key];
 };
+
+
+// 706. Design HashMap
+var MyHashMap = function () {
+  this.data = new Array(1000001);
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+MyHashMap.prototype.put = function (key, value) {
+  this.data[key] = value;
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+MyHashMap.prototype.get = function (key) {
+  const val = this.data[key];
+  return val !== undefined ? val : -1;
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashMap.prototype.remove = function (key) {
+  this.data[key] = undefined;
+};
