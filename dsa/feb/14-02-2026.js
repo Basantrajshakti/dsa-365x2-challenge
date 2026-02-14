b4f7b3584e450cfa23826c230b70cb70c76631b7
Basantraj.shakti <basantrajshakti@gmail.com>
2026-02-14 22:54:14 +0530

    888. Fair Candy Swap

--- dsa/feb/14-02-2026.js
+++ dsa/feb/14-02-2026.js
@@ -25,3 +25,33 @@ var dominantIndex = function (nums) {
   if (max1 >= max2 * 2) return maxIdx;
   return -1;
 };
+
+
+// 888. Fair Candy Swap
+/**
+ * @param {number[]} aliceSizes
+ * @param {number[]} bobSizes
+ * @return {number[]}
+ */
+var fairCandySwap = function (aliceSizes, bobSizes) {
+  // => SumA - x + y = SumB - y + x
+  // => y = x + (SumB - SumA)/2
+  let sumA = 0, sumB = 0;
+
+  for (let i = 0; i < aliceSizes.length; i++) {
+    sumA += aliceSizes[i];
+  }
+
+  for (let i = 0; i < bobSizes.length; i++) {
+    sumB += bobSizes[i];
+  }
+
+  const delta = (sumB - sumA) / 2;
+  for (let i = 0; i < aliceSizes.length; i++) {
+    for (let j = 0; j < bobSizes.length; j++) {
+      if (bobSizes[j] - aliceSizes[i] === delta) {
+        return [aliceSizes[i], bobSizes[j]];
+      }
+    }
+  }
+};
