1ae8870c2bc289990dbe3a1480f8a95940a3a03e
Basantraj.shakti <basantrajshakti@gmail.com>
2026-02-16 18:26:36 +0530

    977. Squares of a Sorted Array

--- dsa/feb/16-02-2026.js
+++ dsa/feb/16-02-2026.js
@@ -15,3 +15,19 @@ var largestPerimeter = function (nums) {
 
   return 0;
 };
+
+
+// 977. Squares of a Sorted Array
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = nums[i] * nums[i];
+  }
+
+  nums.sort((a, b) => a - b);
+
+  return nums;
+};
