79fd9e1213876c8672e1a34b8ecbcc861611a74f
Basantraj.shakti <basantrajshakti@gmail.com>
2026-02-19 19:07:23 +0530

    1854. Maximum Population Year optimized

--- dsa/feb/19-02-2026.js
+++ dsa/feb/19-02-2026.js
@@ -44,3 +44,32 @@ var maximumPopulation = function (logs) {
 
   return maxYear + 1950;
 };
+
+// With optimizations
+/**
+ * @param {number[][]} logs
+ * @return {number}
+ */
+var maximumPopulation = function (logs) {
+  const populationChanges = new Int32Array(101);
+
+  for (const log of logs) {
+    populationChanges[log[0] - 1950]++;
+    populationChanges[log[1] - 1950]--;
+  }
+
+  let currPopulation = 0;
+  let maxPopulation = 0;
+  let maxPopulationYear = 1950;
+
+  for (let i = 0; i < 101; i++) {
+    currPopulation += populationChanges[i];
+
+    if (currPopulation > maxPopulation) {
+      maxPopulation = currPopulation;
+      maxPopulationYear = i + 1950;
+    }
+  }
+
+  return maxPopulationYear;
+};
