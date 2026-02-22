6980273378ef005923ae0d58503166145e8ccf25
Basantraj.shakti <basantrajshakti@gmail.com>
2026-02-22 22:49:55 +0530

    2574. Left and Right Sum Differences optimized

--- dsa/feb/22-02-2026.js
+++ dsa/feb/22-02-2026.js
@@ -24,3 +24,26 @@ var leftRightDifference = function (nums) {
 
   return ans;
 };
+
+// With optimizations
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var leftRightDifference = function (nums) {
+  let tSum = 0, lSum = 0;
+  const len = nums.length;
+  const ans = [];
+
+  for (let i = 0; i < len; i++) {
+    tSum += nums[i];
+  }
+
+  for (let i = 0; i < len; i++) {
+    ans[i] = Math.abs(tSum - lSum - nums[i]);
+    lSum += nums[i];
+    tSum -= nums[i];
+  }
+
+  return ans;
+};
