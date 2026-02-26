f74629c4daa34c1acac8fd1f0fbcdacec0a34483
Basantraj.shakti <basantrajshakti@gmail.com>
2026-02-26 22:47:34 +0530

    83. Remove Duplicates from Sorted List

--- dsa/feb/26-02-2026.js
+++ dsa/feb/26-02-2026.js
@@ -36,3 +36,25 @@ var mergeTwoLists = function (list1, list2) {
 
   return dummyNode.next;
 };
+
+
+// 83. Remove Duplicates from Sorted List
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  let temp = head;
+  while (temp != null && temp.next != null) {
+    if (temp.val == temp.next.val) temp.next = temp.next.next;
+    else temp = temp.next
+  }
+  return head;
+};
