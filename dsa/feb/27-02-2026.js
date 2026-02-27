2eb0315f525c4735259ac0abd251153d7d031dea
Basantraj.shakti <basantrajshakti@gmail.com>
2026-02-27 18:55:55 +0530

    160. Intersection of Two Linked Lists

--- dsa/feb/27-02-2026.js
+++ dsa/feb/27-02-2026.js
@@ -28,3 +28,30 @@ var hasCycle = function (head) {
   }
   return false;
 };
+
+// 160. Intersection of Two Linked Lists
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+  if (!headA || !headB) return null;
+
+  let a = headA, b = headB;
+
+  while (a !== b) {
+    a = a ? a.next : headB;
+    b = b ? b.next : headA;
+  }
+
+  return a;
+};
