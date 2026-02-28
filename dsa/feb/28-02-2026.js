361e5ae3f3a122a08f804b44d1d9072e1bb7b775
Basantraj.shakti <basantrajshakti@gmail.com>
2026-02-28 21:32:12 +0530

    206. Reverse Linked List

--- dsa/feb/28-02-2026.js
+++ dsa/feb/28-02-2026.js
@@ -45,3 +45,28 @@ var removeElements = function (head, val) {
   }
   return dummy.next;
 };
+
+
+// 206. Reverse Linked List
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let prev = null;
+  let curr = head;
+  while (curr !== null) {
+    let nextTemp = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = nextTemp;
+  }
+  return prev;
+};
