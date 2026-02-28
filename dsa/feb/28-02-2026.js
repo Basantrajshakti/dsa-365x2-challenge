cca0cd8f60faa785988fb287d1a472144b0cac07
Basantraj.shakti <basantrajshakti@gmail.com>
2026-02-28 21:22:07 +0530

    203. Remove Linked List Elements optimized

--- dsa/feb/28-02-2026.js
+++ dsa/feb/28-02-2026.js
@@ -22,3 +22,26 @@ var removeElements = function (head, val) {
   }
   return temp.next;
 };
+
+// With optimizations
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function (head, val) {
+  let dummy = new ListNode(-1);
+  dummy.next = head;
+  let prev = dummy;
+  let cur = head;
+  while (cur) {
+    if (cur.val === val) {
+      prev.next = cur.next;
+      cur = cur.next;
+    } else {
+      prev = cur;
+      cur = cur.next;
+    }
+  }
+  return dummy.next;
+};
