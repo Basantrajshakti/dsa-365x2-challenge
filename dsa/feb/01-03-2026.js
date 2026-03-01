c79346eed5484e815d78bc043ad85f453184d8d0
Basantraj.shakti <basantrajshakti@gmail.com>
2026-03-01 21:41:02 +0530

    234. Palindrome Linked List

---
+++ dsa/feb/01-03-2026.js
@@ -0,0 +1,55 @@
+//  linked list
+
+
+// 234. Palindrome Linked List
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function (head) {
+  if (!head || !head.next) {
+    return true;
+  }
+
+  let slow = head;
+  let fast = head;
+
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  slow.next = reverseList(slow.next);
+
+  slow = slow.next;
+
+  while (slow) {
+    if (head.val !== slow.val) {
+      return false;
+    }
+    head = head.next;
+    slow = slow.next;
+  }
+  return true;
+};
+
+function reverseList(head) {
+  let pre = null;
+  let next = null;
+
+  while (head) {
+    next = head.next;
+    head.next = pre;
+    pre = head;
+    head = next;
+  }
+  return pre;
+}
