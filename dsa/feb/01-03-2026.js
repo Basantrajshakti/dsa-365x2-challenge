eea1c109e94f075d26fa1c6b2380ce00cfb16017
Basantraj.shakti <basantrajshakti@gmail.com>
2026-03-01 21:50:24 +0530

    705. Design HashSet

--- dsa/feb/01-03-2026.js
+++ dsa/feb/01-03-2026.js
@@ -69,3 +69,41 @@ var isPalindrome = function (head) {
   }
   return str === revStr;
 };
+
+
+// 705. Design HashSet
+var MyHashSet = function () {
+  this.data = new Array(1000001);
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.add = function (key) {
+  this.data[key] = true;
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashSet.prototype.remove = function (key) {
+  this.data[key] = false;
+};
+
+/** 
+ * @param {number} key
+ * @return {boolean}
+ */
+MyHashSet.prototype.contains = function (key) {
+  return !!this.data[key];
+};
+
+/** 
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
