fcd5c9e005f01231aba2ead9c2515ceb37fb1794
Basantraj.shakti <basantrajshakti@gmail.com>
2026-03-02 20:48:37 +0530

    706. Design HashMap

---
+++ dsa/march/02-03-2026.js
@@ -0,0 +1,41 @@
+// Linked list
+
+// 706. Design HashMap
+
+var MyHashMap = function () {
+  this.data = new Array(1000001);
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+MyHashMap.prototype.put = function (key, value) {
+  this.data[key] = value;
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+MyHashMap.prototype.get = function (key) {
+  const val = this.data[key];
+  return val !== undefined ? val : -1;
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashMap.prototype.remove = function (key) {
+  this.data[key] = undefined;
+};
+
+/** 
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
