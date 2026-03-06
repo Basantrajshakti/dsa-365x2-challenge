815d967d8acb077b3943d6e4f0a76f11e97424f8
Basantraj.shakti <basantrajshakti@gmail.com>
2026-03-06 21:39:46 +0530

    566. Reshape the Matrix

--- dsa/march/06-03-2026.js
+++ dsa/march/06-03-2026.js
@@ -12,3 +12,36 @@ var checkOnesSegment = function (s) {
 
   return true;
 };
+
+
+// Arrays
+
+// 566. Reshape the Matrix
+/**
+ * @param {number[][]} mat
+ * @param {number} r
+ * @param {number} c
+ * @return {number[][]}
+ */
+var matrixReshape = function (mat, r, c) {
+  if (mat.length * mat[0].length !== r * c) return mat;
+
+  let mat1d = [], rmat = [];
+
+  for (let i = 0; i < mat.length; i++) {
+    for (let j = 0; j < mat[i].length; j++) {
+      mat1d.push(mat[i][j]);
+    }
+  }
+
+  let curr = 0;
+  for (let i = 0; i < r; i++) {
+    let arr = [];
+    for (let j = 0; j < c; j++) {
+      arr.push(mat1d[curr++]);
+    }
+    rmat.push(arr);
+  }
+
+  return rmat;
+};
