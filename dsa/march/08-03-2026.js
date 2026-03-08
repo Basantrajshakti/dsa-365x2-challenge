314efd7b54233a71588b95bff514b41af82c305d
Basantraj.shakti <basantrajshakti@gmail.com>
2026-03-08 22:31:31 +0530

    682. Baseball Game

--- dsa/march/08-03-2026.js
+++ dsa/march/08-03-2026.js
@@ -14,3 +14,36 @@ var findDifferentBinaryString = function (nums) {
 
   return ans;
 };
+
+
+// 682. Baseball Game
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var calPoints = function (operations) {
+  let arr = [], arrIdx = 0;
+
+  for (let i = 0; i < operations.length; i++) {
+    if (operations[i] === 'C') {
+      if (arr.length !== 0) {
+        arr.length = arr.length - 1;
+        arrIdx -= 2;
+      }
+    } else if (operations[i] === 'D') {
+      arr.push((arr[arrIdx - 1] || 0) * 2);
+    } else if (operations[i] === '+') {
+      arr.push((arr[arrIdx - 1] || 0) + (arr[arrIdx - 2] || 0));
+    } else {
+      arr.push(+operations[i]);
+    }
+    arrIdx++;
+  }
+
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += +arr[i];
+  }
+
+  return sum;
+};
