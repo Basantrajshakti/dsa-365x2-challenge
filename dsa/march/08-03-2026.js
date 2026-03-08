de023444263e7c13ba72281eb9f6770ff0f330ef
Basantraj.shakti <basantrajshakti@gmail.com>
2026-03-08 22:03:33 +0530

    1980. Find Unique Binary String

---
+++ dsa/march/08-03-2026.js
@@ -0,0 +1,16 @@
+// Arrays
+
+// 1980. Find Unique Binary String
+/**
+ * @param {string[]} nums
+ * @return {string}
+ */
+var findDifferentBinaryString = function (nums) {
+  let ans = "";
+
+  for (let i = 0; i < nums.length; i++) {
+    ans += nums[i][i] === '0' ? '1' : '0';
+  }
+
+  return ans;
+};
