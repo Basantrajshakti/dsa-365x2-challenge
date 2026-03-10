670f8c26b1688bfb315e58266f7340c9e7e8bc8e
Basantraj.shakti <basantrajshakti@gmail.com>
2026-03-10 21:31:46 +0530

    744. Find Smallest Letter Greater Than Target optimized

--- dsa/march/10-03-2026.js
+++ dsa/march/10-03-2026.js
@@ -16,3 +16,28 @@ var nextGreatestLetter = function (letters, target) {
   }
   return max === '~' ? letters[0] : max;
 };
+
+// With optimizations
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function (letters, target) {
+  let n = letters.length;
+  let low = 0, high = n - 1;
+  let ans = letters[0];
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (letters[mid] > target) {
+      ans = letters[mid];
+      high = mid - 1;
+    }
+    else {
+      low = mid + 1;
+    }
+  }
+  return ans;
+};
