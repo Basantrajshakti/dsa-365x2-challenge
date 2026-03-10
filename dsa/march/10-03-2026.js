20fcd6630efa247033ac2291a6025923cb495e09
Basantraj.shakti <basantrajshakti@gmail.com>
2026-03-10 22:09:45 +0530

    746. Min Cost Climbing Stairs optimized

--- dsa/march/10-03-2026.js
+++ dsa/march/10-03-2026.js
@@ -60,3 +60,18 @@ var minCostClimbingStairs = function (cost) {
   }
   return dp1;
 };
+
+// With optimizations
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function (cost) {
+  cost.push(0);
+
+  for (let i = cost.length - 4; i >= 0; i--) {
+    cost[i] += Math.min(cost[1 + i], cost[i + 2])
+  }
+
+  return Math.min(cost[0], cost[1])
+};
