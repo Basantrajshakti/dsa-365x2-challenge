3473bff74466820e779c24005854d29acad2f19e
Basantraj.shakti <basantrajshakti@gmail.com>
2026-03-11 21:10:32 +0530

    1009. Complement of Base 10 Integer optimized

--- dsa/march/11-03-2026.js
+++ dsa/march/11-03-2026.js
@@ -19,3 +19,15 @@ var bitwiseComplement = function (n) {
 
   return n ^ mask;
 };
+
+// With optimizations
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var bitwiseComplement = function (n) {
+  if (n === 0) return 1;
+  const bitLength = Math.floor(Math.log2(n)) + 1;
+  const mask = (1 << bitLength) - 1;
+  return n ^ mask;
+};
