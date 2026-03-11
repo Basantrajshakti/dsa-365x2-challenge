28bf8b9cf139972cba37a4ebee20a10e38435a87
Basantraj.shakti <basantrajshakti@gmail.com>
2026-03-11 20:59:54 +0530

    1009. Complement of Base 10 Integer

---
+++ dsa/march/11-03-2026.js
@@ -0,0 +1,21 @@
+// Daily Challenge
+
+
+// 1009. Complement of Base 10 Integer
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var bitwiseComplement = function (n) {
+  if (n === 0) return 1;
+
+  let temp = n;
+  let mask = 0;
+
+  while (temp > 0) {
+    mask = (mask << 1) | 1;
+    temp >>= 1;
+  }
+
+  return n ^ mask;
+};
