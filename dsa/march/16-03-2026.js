dc02bc4bbdc07706503c73b42dc45c8e0b77a8e3
Basantraj.shakti <basantrajshakti@gmail.com>
2026-03-16 20:50:06 +0530

    914. X of a Kind in a Deck of Cards

--- dsa/march/16-03-2026.js
+++ dsa/march/16-03-2026.js
@@ -14,3 +14,29 @@ var smallestRangeI = function (nums, k) {
 
   return diff > 0 ? diff : 0;
 };
+
+
+// 914. X of a Kind in a Deck of Cards
+/**
+ * @param {number[]} deck
+ * @return {boolean}
+ */
+var hasGroupsSizeX = function (deck) {
+  var count = {};
+  deck.map((x) => (count[x] ? (count[x] += 1) : (count[x] = 1)));
+
+  var arr = Object.values(count);
+  var num = arr[0];
+
+  for (const i of arr) {
+    num = gcd(num, i);
+    if (num < 2) {
+      return false;
+    }
+  }
+  return num > 1;
+};
+
+var gcd = function (a, b) {
+  return a == 0 ? b : gcd(b % a, a);
+};
