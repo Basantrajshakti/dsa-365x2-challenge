92f1f4198124f26f53db7d182a6d83dec0e01a98
Basantraj.shakti <basantrajshakti@gmail.com>
2026-03-16 19:50:07 +0530

    908. Smallest Range I

---
+++ dsa/march/16-03-2026.js
@@ -0,0 +1,16 @@
+// Arrays
+
+// 908. Smallest Range I
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var smallestRangeI = function (nums, k) {
+  let min = Math.min(...nums);
+  let max = Math.max(...nums);
+
+  let diff = max - min - 2 * k;
+
+  return diff > 0 ? diff : 0;
+};
