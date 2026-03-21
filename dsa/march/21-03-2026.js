0eb2a56a185acf85d25e4e43919537eff0a72cf3
Basantraj.shakti <basantrajshakti@gmail.com>
2026-03-21 20:38:12 +0530

    521. Longest Uncommon Subsequence I

--- dsa/march/21-03-2026.js
+++ dsa/march/21-03-2026.js
@@ -24,3 +24,14 @@ var reverseSubmatrix = function (grid, x, y, k) {
 
   return grid;
 };
+
+
+// 521. Longest Uncommon Subsequence I
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {number}
+ */
+var findLUSlength = function (a, b) {
+  return a === b ? -1 : Math.max(a.length, b.length);
+};
