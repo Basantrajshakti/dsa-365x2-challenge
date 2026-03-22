540f6809b8840614400174d4b7ad27a8c02743a7
Basantraj.shakti <basantrajshakti@gmail.com>
2026-03-22 19:37:21 +0530

    1758. Minimum Changes To Make Alternating Binary String optimized

--- dsa/march/22-03-2026.js
+++ dsa/march/22-03-2026.js
@@ -94,3 +94,19 @@ var minOperations = function (s) {
 
   return Math.min(opt1, opt2);
 };
+
+// With optimizations
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minOperations = function (s) {
+  let count1 = 0, count2 = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== (i % 2 === 0 ? '0' : '1')) count1++;
+    if (s[i] !== (i % 2 === 0 ? '1' : '0')) count2++;
+  }
+
+  return Math.min(count1, count2);
+};
