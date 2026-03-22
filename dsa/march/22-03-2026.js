4f94d4f9bdaf7c38cc1f2b72538e3980c8843d61
Basantraj.shakti <basantrajshakti@gmail.com>
2026-03-22 19:13:28 +0530

    1886. Determine Whether Matrix Can Be Obtained By Rotation optimized

--- dsa/march/22-03-2026.js
+++ dsa/march/22-03-2026.js
@@ -41,3 +41,31 @@ var findRotation = function (mat, target) {
 };
 
 
+// With optimizations
+/**
+ * @param {number[][]} mat
+ * @param {number[][]} target
+ * @return {boolean}
+ */
+var findRotation = function (mat, target) {
+
+  const res = Array(4).fill(true);
+  const n = mat.length;
+
+  for (let r = 0; r < n; r++) {
+    for (let c = 0; c < n; c++) {
+
+      if (target[r][c] != mat[r][c])
+        res[0] = false;
+      if (target[r][c] != mat[c][n - 1 - r])
+        res[1] = false;
+      if (target[r][c] != mat[n - 1 - r][n - 1 - c])
+        res[2] = false;
+      if (target[r][c] != mat[n - 1 - c][r])
+        res[3] = false;
+
+    }
+  }
+
+  return res[0] || res[1] || res[2] || res[3];
+};
