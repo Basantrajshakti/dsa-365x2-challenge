c78ea7629c86fdbd596ef5fb7f1479c4187a5030
Basantraj.shakti <basantrajshakti@gmail.com>
2026-03-23 13:00:42 +0530

    1582. Special Positions in a Binary Matrix

--- dsa/march/23-03-2026.js
+++ dsa/march/23-03-2026.js
@@ -39,3 +39,37 @@ var maxProductPath = function (grid) {
   let res = maxDp[m - 1][n - 1];
   return res < 0 ? -1 : res % MOD;
 };
+
+
+// 1582. Special Positions in a Binary Matrix
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var numSpecial = function (mat) {
+  let m = mat.length;
+  let n = mat[0].length;
+
+  let rowcnt = new Array(m).fill(0);
+  let colcnt = new Array(n).fill(0);
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (mat[i][j] === 1) {
+        rowcnt[i]++;
+        colcnt[j]++;
+      }
+    }
+  }
+
+  let ans = 0;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (mat[i][j] === 1 && rowcnt[i] === 1 && colcnt[j] === 1) {
+        ans++;
+      }
+    }
+  }
+
+  return ans;
+};
