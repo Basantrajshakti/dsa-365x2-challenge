59a790d44230d709344feb9498aaefaafb67c14f
Basantraj.shakti <basantrajshakti@gmail.com>
2026-03-26 17:37:32 +0530

    1689. Partitioning Into Minimum Number Of Deci-Binary Numbers

--- dsa/march/26-03-2026.js
+++ dsa/march/26-03-2026.js
@@ -69,3 +69,27 @@ var canPartitionGrid = function (grid) {
   }
   return false;
 };
+
+
+// 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers
+/**
+ * @param {string} n
+ * @return {number}
+ */
+var minPartitions = function (n) {
+  let maxDigit = 0;
+
+  for (let i = 0; i < n.length; i++) {
+    let digit = n[i] - '0';
+
+    if (digit > maxDigit) {
+      maxDigit = digit;
+    }
+
+    if (maxDigit === 9) {
+      break;
+    }
+  }
+
+  return maxDigit;
+};
