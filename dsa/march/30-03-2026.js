073762f5bc3bc8d7d2e9929567f451d69d26a7d3
Basantraj.shakti <basantrajshakti@gmail.com>
2026-03-30 23:57:01 +0530

    557. Reverse Words in a String III one liner

--- dsa/march/30-03-2026.js
+++ dsa/march/30-03-2026.js
@@ -67,3 +67,15 @@ var reverseWords = function (s) {
   }
   return res + word;
 };
+
+// One liner
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function (s) {
+  return s
+    .split(" ")
+    .map((w) => w.split("").reverse().join(""))
+    .join(" ");
+};
