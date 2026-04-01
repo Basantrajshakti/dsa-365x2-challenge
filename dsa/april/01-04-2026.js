18c5050ed9bac42b46c62afa249aed3cc1e7fd61
Basantraj.shakti <basantrajshakti@gmail.com>
2026-04-01 23:49:34 +0530

    657. Robot Return to Origin

--- dsa/april/01-04-2026.js
+++ dsa/april/01-04-2026.js
@@ -51,3 +51,31 @@ var survivedRobotsHealths = function (pos, h, d) {
 
     return res;
 };
+
+
+// 657. Robot Return to Origin
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function (moves) {
+    let x = 0;
+    let y = 0;
+    let ans = moves.split('');
+
+    for (let i = 0; i < ans.length; i++) {
+        if (ans[i] == 'U') {
+            x++;
+        } else if (ans[i] == 'D') {
+            x--;
+        } else if (ans[i] == 'R') {
+            y++;
+        } else if (ans[i] == 'L') {
+            y--;
+        }
+    }
+    if (x == 0 && y == 0) {
+        return true;
+    }
+    return false;
+};
