972141f5bfefa34bdfd96917e713c32105b2bb13
Basantraj.shakti <basantrajshakti@gmail.com>
2026-04-02 23:48:51 +0530

    680. Valid Palindrome II

--- dsa/april/02-04-2026.js
+++ dsa/april/02-04-2026.js
@@ -34,3 +34,30 @@ var maximumAmount = function (coins) {
 
     return dp[n][2];
 };
+
+
+// 680. Valid Palindrome II
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var validPalindrome = function (s) {
+    const isPalindrome = (left, right) => {
+        while (left < right) {
+            if (s[left] !== s[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    };
+
+    let left = 0, right = s.length - 1;
+    while (left < right) {
+        if (s[left] !== s[right]) {
+            return isPalindrome(left + 1, right) || isPalindrome(left, right - 1);
+        }
+        left++;
+        right--;
+    }
+    return true;
+};
