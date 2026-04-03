badb4e2d39a6ccb45508ebf54082038b5684c4c1
Basantraj.shakti <basantrajshakti@gmail.com>
2026-04-03 23:13:11 +0530

    696. Count Binary Substrings

--- dsa/april/03-04-2026.js
+++ dsa/april/03-04-2026.js
@@ -77,3 +77,26 @@ var maxWalls = function(robots, distance, walls) {
 
     return Math.max(dp[n - 1][0], dp[n - 1][1]);
 };
+
+
+// 696. Count Binary Substrings
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const countBinarySubstrings = s => {
+    let res = 0, prev = 0, strk = 1;
+
+    for (let i = 1; i < s.length; i++) {
+        if (s[i] === s[i - 1])
+            strk++;
+        else {
+            prev = strk;
+            strk = 1;
+        }
+
+        if (strk <= prev) res++;
+    }
+
+    return res;
+};
