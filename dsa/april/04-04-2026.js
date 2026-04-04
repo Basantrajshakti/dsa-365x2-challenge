6e35dade6e90c1060e76f215c8827912613d1635
Basantraj.shakti <basantrajshakti@gmail.com>
2026-04-04 08:58:17 +0530

    709. To Lower Case

--- dsa/april/04-04-2026.js
+++ dsa/april/04-04-2026.js
@@ -26,3 +26,21 @@ const decodeCiphertext = (encodedText, rows) => {
 };
 
 
+// 709. To Lower Case
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function (s) {
+    let result = '';
+
+    for (let i = 0; i < s.length; i++) {
+        const ascii = s.charCodeAt(i);
+        if (ascii >= 65 && ascii <= 90) {
+            result += String.fromCharCode(ascii + 32);
+        } else {
+            result += s.charAt(i);
+        }
+    }
+    return result;
+};
