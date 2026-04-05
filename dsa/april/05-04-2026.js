5eb3285b6712ac10d2d3665eb4b631d5ab9171c5
Basantraj.shakti <basantrajshakti@gmail.com>
2026-04-05 23:08:25 +0530

    748. Shortest Completing Word

--- dsa/april/05-04-2026.js
+++ dsa/april/05-04-2026.js
@@ -26,3 +26,30 @@ var judgeCircle = function (moves) {
     }
     return false;
 };
+
+
+// 748. Shortest Completing Word
+/**
+ * @param {string} licensePlate
+ * @param {string[]} words
+ * @return {string}
+ */
+var shortestCompletingWord = function (licensePlate, words) {
+    let fixLicense = licensePlate.toLowerCase().replace(/[\d\s]/g, '');
+    let minWord = '';
+    let minWordLen = Infinity;
+
+    for (let word of words) {
+        let copyLicense = fixLicense;
+
+        for (let i = 0; i < word.length; i++) {
+            copyLicense = copyLicense.replace(word[i], '');
+            if (!copyLicense && word.length < minWordLen){
+                minWord = word;
+                minWordLen = word.length;
+            }
+        }
+    }
+
+    return minWord;
+};
