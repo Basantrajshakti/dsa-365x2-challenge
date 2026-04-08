c74dc3f8069896474af1fa7b75b4afb2ae212ace
Basantraj.shakti <basantrajshakti@gmail.com>
2026-04-08 20:48:44 +0530

    804. Unique Morse Code Words

--- dsa/april/08-04-2026.js
+++ dsa/april/08-04-2026.js
@@ -22,3 +22,50 @@ var xorAfterQueries = function (nums, queries) {
 
   return res;
 };
+
+// 804. Unique Morse Code Words
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+const map = {
+  a: ".-",
+  b: "-...",
+  c: "-.-.",
+  d: "-..",
+  e: ".",
+  f: "..-.",
+  g: "--.",
+  h: "....",
+  i: "..",
+  j: ".---",
+  k: "-.-",
+  l: ".-..",
+  m: "--",
+  n: "-.",
+  o: "---",
+  p: ".--.",
+  q: "--.-",
+  r: ".-.",
+  s: "...",
+  t: "-",
+  u: "..-",
+  v: "...-",
+  w: ".--",
+  x: "-..-",
+  y: "-.--",
+  z: "--..",
+};
+
+var uniqueMorseRepresentations = function (words) {
+  const set = new Set();
+
+  for (const word of words) {
+    let str = "";
+    for (const char of word) {
+      str += map[char];
+    }
+    set.add(str);
+  }
+  return set.size;
+};
