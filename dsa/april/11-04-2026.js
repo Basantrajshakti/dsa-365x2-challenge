185be73ad2f97820720c79df8fd2bd2166820d5c
Basantraj.shakti <basantrajshakti@gmail.com>
2026-04-11 23:36:35 +0530

    3741. Minimum Distance Between Three Equal Elements II

---
+++ dsa/april/11-04-2026.js
@@ -0,0 +1,27 @@
+// Daily challenge
+
+// 3741. Minimum Distance Between Three Equal Elements II
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumDistance = function (nums) {
+  const n = nums.length;
+  const last = new Array(n + 1).fill(-1);
+  const second = new Array(n + 1).fill(-1);
+  let minDist = Infinity;
+
+  for (let i = 0; i < n; i++) {
+    const num = nums[i];
+    if (second[num] !== -1) {
+      const dist = i - second[num];
+      if (dist < minDist) {
+        minDist = dist;
+      }
+    }
+    second[num] = last[num];
+    last[num] = i;
+  }
+
+  return minDist === Infinity ? -1 : 2 * minDist;
+};
