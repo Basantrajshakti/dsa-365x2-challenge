075611e32bf9c85099be29f0f8ead8e5b541ef30
Basantraj.shakti <basantrajshakti@gmail.com>
2026-04-12 23:07:33 +0530

    1320. Minimum Distance to Type a Word Using Two Fingers

---
+++ dsa/april/12-04-2026.js
@@ -0,0 +1,31 @@
+// Daily challenge
+
+// 1320. Minimum Distance to Type a Word Using Two Fingers
+/**
+ * @param {string} word
+ * @return {number}
+ */
+var minimumDistance = function (word) {
+  const dist = (a, b) => {
+    if (a === 26 || b === 26) return 0;
+    return Math.abs(Math.floor(a / 6) - Math.floor(b / 6)) + Math.abs((a % 6) - (b % 6));
+  };
+
+  let dp = Array(27).fill(Infinity);
+  dp[26] = 0;
+  let prev = word.charCodeAt(0) - 65;
+
+  for (let i = 1; i < word.length; i++) {
+    let cur = word.charCodeAt(i) - 65;
+    let next_dp = Array(27).fill(Infinity);
+    for (let free = 0; free < 27; free++) {
+      if (dp[free] === Infinity) continue;
+      next_dp[free] = Math.min(next_dp[free], dp[free] + dist(prev, cur));
+      next_dp[prev] = Math.min(next_dp[prev], dp[free] + dist(free, cur));
+    }
+    dp = next_dp;
+    prev = cur;
+  }
+
+  return Math.min(...dp);
+};
