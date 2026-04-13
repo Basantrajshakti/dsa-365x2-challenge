2fed46c0907941bb108679396cce633e6ac41109
Basantraj.shakti <basantrajshakti@gmail.com>
2026-04-13 18:40:14 +0530

    66. Plus One

--- dsa/april/13-04-2026.js
+++ dsa/april/13-04-2026.js
@@ -16,3 +16,21 @@ var getMinDistance = function (nums, target, start) {
   }
   return mindist;
 };
+
+// 66. Plus One
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] === 9) {
+      digits[i] = 0;
+    } else {
+      digits[i] += 1;
+      return digits;
+    }
+  }
+
+  return [1, ...digits];
+};
