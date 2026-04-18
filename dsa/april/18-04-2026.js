f23c55f03014458b288bba2dc4e53a479f8602ba
Basantraj.shakti <basantrajshakti@gmail.com>
2026-04-18 22:42:04 +0530

    121. Best Time to Buy and Sell Stock

--- dsa/april/18-04-2026.js
+++ dsa/april/18-04-2026.js
@@ -30,3 +30,22 @@ var mirrorDistance = function (n) {
   };
   return Math.abs(n - getReverse(n));
 };
+
+// 121. Best Time to Buy and Sell Stock
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let maxProfit = 0;
+  let minSoFar = prices[0];
+  const len = prices.length;
+
+  for (let i = 1; i < len; i++) {
+    const price = prices[i];
+    minSoFar = Math.min(minSoFar, price);
+    maxProfit = Math.max(maxProfit, price - minSoFar);
+  }
+
+  return maxProfit;
+};
