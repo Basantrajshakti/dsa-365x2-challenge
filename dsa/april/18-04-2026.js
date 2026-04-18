50c76e29f21dc58e6fccdb6225783c2a5a3292c4
Basantraj.shakti <basantrajshakti@gmail.com>
2026-04-18 22:22:22 +0530

    3783. Mirror Distance of an Integer optimized

--- dsa/april/18-04-2026.js
+++ dsa/april/18-04-2026.js
@@ -13,3 +13,20 @@ var mirrorDistance = function (n) {
   };
   return Math.abs(n - revnum(n));
 };
+
+// With optimizations
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var mirrorDistance = function (n) {
+  const getReverse = () => {
+    let rev = 0;
+    while (n > 0) {
+      rev = rev * 10 + (n % 10);
+      n = Math.floor(n / 10);
+    }
+    return rev;
+  };
+  return Math.abs(n - getReverse(n));
+};
