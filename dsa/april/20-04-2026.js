1161ab28f4126ba8a89eb713495190be994caf85
Basantraj.shakti <basantrajshakti@gmail.com>
2026-04-20 23:41:47 +0530

    2078. Two Furthest Houses With Different Colors

---
+++ dsa/april/20-04-2026.js
@@ -0,0 +1,26 @@
+// Daily challenge
+
+// 2078. Two Furthest Houses With Different Colors
+/**
+ * @param {number[]} colors
+ * @return {number}
+ */
+const maxDistance = (A) => {
+  const n = A.length;
+  let left = 0,
+    right = 0;
+
+  for (let i = 0; i < n; i++)
+    if (A[i] ^ A.at(-1)) {
+      left = i;
+      break;
+    }
+
+  for (let i = n - 1; i >= 0; i--)
+    if (A[i] ^ A[0]) {
+      right = i;
+      break;
+    }
+
+  return Math.max(n - 1 - left, right);
+};
