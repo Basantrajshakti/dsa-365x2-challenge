29a8d36c8c817be0147e75a518f3182b9ba4ff4a
Basantraj.shakti <basantrajshakti@gmail.com>
2026-04-22 19:44:48 +0530

    219. Contains Duplicate II

--- dsa/april/22-04-2026.js
+++ dsa/april/22-04-2026.js
@@ -32,3 +32,24 @@ var twoEditWords = function (queries, dictionary) {
 
   return result;
 };
+
+// 219. Contains Duplicate II
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    if (num in map && Math.abs(map[num] - i) <= k) {
+      return true;
+    }
+
+    map[num] = i;
+  }
+
+  return false;
+};
