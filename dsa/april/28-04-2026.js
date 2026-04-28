95edf5e110916d327ca35075a158afb6ee92d122
Basantraj.shakti <basantrajshakti@gmail.com>
2026-04-28 09:47:26 +0530

    2033. Minimum Operations to Make a Uni-Value Grid optimized

--- dsa/april/28-04-2026.js
+++ dsa/april/28-04-2026.js
@@ -31,3 +31,41 @@ var minOperations = function (grid, x) {
 
   return ops;
 };
+
+// With optimizations
+/**
+ * @param {number[][]} grid
+ * @param {number} x
+ * @return {number}
+ */
+var minOperations = function (grid, x) {
+  const n = grid.length;
+  const m = grid[0].length;
+  const nm = n * m;
+  const count = new Uint16Array(10001);
+  const r = grid[0][0] % x;
+
+  for (let i = 0; i < n; ++i) {
+    const row = grid[i];
+    for (let j = 0; j < m; ++j) {
+      const y = row[j];
+      if (r === y % x) ++count[(y / x) >> 0];
+      else return -1;
+    }
+  }
+
+  let y = 0;
+  const mi = (nm + 1) >> 1;
+
+  for (let i = 0; i < mi; i += count[y++]);
+
+  --y;
+  let cnt = 0;
+  const maxq = (10000 / x) >> 0;
+
+  for (let i = 0; i <= maxq; ++i) {
+    cnt += count[i] * Math.abs(i - y);
+  }
+
+  return cnt;
+};
