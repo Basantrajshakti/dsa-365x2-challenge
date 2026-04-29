ff1ffcebf78bb4d402d019ccb6103c88eac8feb4
Basantraj.shakti <basantrajshakti@gmail.com>
2026-04-29 22:18:53 +0530

    414. Third Maximum Number

--- dsa/april/29-04-2026.js
+++ dsa/april/29-04-2026.js
@@ -36,3 +36,31 @@ var maximumScore = function (grid) {
 
   return Math.max(...dp1);
 };
+
+// 414. Third Maximum Number
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function (nums) {
+  let max1 = -Infinity,
+    max2 = -Infinity,
+    max3 = -Infinity;
+
+  for (const num of nums) {
+    if (num > max1) {
+      max3 = max2;
+      max2 = max1;
+      max1 = num;
+    } else if (num > max2 && num < max1) {
+      max3 = max2;
+      max2 = num;
+    } else if (num > max3 && num < max2) {
+      max3 = num;
+    }
+  }
+
+  if (max3 == -Infinity) return max1;
+
+  return max3;
+};
