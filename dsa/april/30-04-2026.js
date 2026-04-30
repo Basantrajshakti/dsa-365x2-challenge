da1c5e5b3d1e16de97f37056dfb4f213c399c758
Basantraj.shakti <basantrajshakti@gmail.com>
2026-04-30 22:42:24 +0530

    3742. Maximum Path Score in a Grid

---
+++ dsa/april/30-04-2026.js
@@ -0,0 +1,49 @@
+// Daily challenge
+
+// 3742. Maximum Path Score in a Grid
+/**
+ * @param {number[][]} grid
+ * @param {number} k
+ * @return {number}
+ */
+var maxPathScore = function (grid, k) {
+  const m = grid.length;
+  const n = grid[0].length;
+  let dp = Array.from({ length: n }, () => new Int32Array(k + 1).fill(-1));
+
+  dp[0][k] = 0;
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      const curr = grid[i][j];
+      const combined = new Int32Array(k + 1).fill(-1);
+
+      for (let rk = 0; rk <= k; rk++) {
+        combined[rk] = Math.max(combined[rk], dp[j][rk]);
+      }
+
+      if (j > 0) {
+        for (let rk = 0; rk <= k; rk++) {
+          combined[rk] = Math.max(combined[rk], dp[j - 1][rk]);
+        }
+      }
+
+      if (curr !== 0) {
+        dp[j].fill(-1);
+        for (let rk = 1; rk <= k; rk++) {
+          if (combined[rk] !== -1) {
+            dp[j][rk - 1] = combined[rk] + curr;
+          }
+        }
+      } else {
+        dp[j] = combined;
+      }
+    }
+  }
+
+  let maxVal = -1;
+  for (let rk = 0; rk <= k; rk++) {
+    maxVal = Math.max(maxVal, dp[n - 1][rk]);
+  }
+  return maxVal;
+};
