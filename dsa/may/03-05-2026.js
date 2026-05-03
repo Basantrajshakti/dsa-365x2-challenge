6ef044bf15593413bfb2f924c8471786b4a33615
Basantraj.shakti <basantrajshakti@gmail.com>
2026-05-03 22:13:58 +0530

    796. Rotate String

---
+++ dsa/may/03-05-2026.js
@@ -0,0 +1,14 @@
+// Daily challenege
+
+// 796. Rotate String
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var rotateString = function (s, goal) {
+  if (s.length !== goal.length) {
+    return false;
+  }
+  return (s + s).includes(goal);
+};
