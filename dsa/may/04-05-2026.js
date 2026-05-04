d54440b53ca9c5ad49d517c1ed7ea207ce010f9c
Basantraj.shakti <basantrajshakti@gmail.com>
2026-05-04 21:14:43 +0530

    495. Teemo Attacking

--- dsa/may/04-05-2026.js
+++ dsa/may/04-05-2026.js
@@ -26,3 +26,23 @@ var rotate = function (matrix) {
     }
   }
 };
+
+// 495. Teemo Attacking
+/**
+ * @param {number[]} timeSeries
+ * @param {number} duration
+ * @return {number}
+ */
+var findPoisonedDuration = function (timeSeries, duration) {
+  if (timeSeries.length === 0) return 0;
+
+  let total = 0;
+
+  for (let i = 0; i < timeSeries.length - 1; i++) {
+    const diff = timeSeries[i + 1] - timeSeries[i];
+    total += Math.min(duration, diff);
+  }
+  // For last value
+  total += duration;
+  return total;
+};
