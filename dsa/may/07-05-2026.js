3799c74dec833d6afa4c3f4a8877ccec4645c31f
Basantraj.shakti <basantrajshakti@gmail.com>
2026-05-07 21:39:27 +0530

    506. Relative Ranks

--- dsa/may/07-05-2026.js
+++ dsa/may/07-05-2026.js
@@ -26,3 +26,27 @@ var maxValue = function (nums) {
   }
   return res;
 };
+
+// 506. Relative Ranks
+/**
+ * @param {number[]} score
+ * @return {string[]}
+ */
+var findRelativeRanks = function (score) {
+  const ans = [...score].sort((a, b) => b - a);
+  const map = {
+    [ans[0]]: "Gold Medal",
+    [ans[1]]: "Silver Medal",
+    [ans[2]]: "Bronze Medal",
+  };
+
+  for (let i = 3; i < ans.length; i++) {
+    map[ans[i]] = i + 1 + "";
+  }
+
+  for (let i = 0; i < score.length; i++) {
+    ans[i] = map[score[i]];
+  }
+
+  return ans;
+};
