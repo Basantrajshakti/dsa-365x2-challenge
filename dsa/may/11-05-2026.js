fd684604710c2a9b501dba63dd7d9626e6fcdc47
Basantraj.shakti <basantrajshakti@gmail.com>
2026-05-11 10:17:38 +0530

    561. Array Partition

--- dsa/may/11-05-2026.js
+++ dsa/may/11-05-2026.js
@@ -18,3 +18,19 @@ var separateDigits = function (nums) {
 
   return result;
 };
+
+// 561. Array Partition
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function (nums) {
+  nums.sort((a, b) => a - b);
+  let sum = 0;
+
+  for (let i = 0; i < nums.length; i += 2) {
+    sum += nums[i];
+  }
+
+  return sum;
+};
