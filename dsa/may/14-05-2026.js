5eb091e045c36a09c06f7c978a415099e3a3f1ca
Basantraj.shakti <basantrajshakti@gmail.com>
2026-05-14 10:53:55 +0530

    594. Longest Harmonious Subsequence

--- dsa/may/14-05-2026.js
+++ dsa/may/14-05-2026.js
@@ -23,3 +23,25 @@ var isGood = function (nums) {
 
   return true;
 };
+
+// 594. Longest Harmonious Subsequence
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLHS = function (nums) {
+  const map = new Map();
+
+  for (num of nums) {
+    map.set(num, (map.get(num) || 0) + 1);
+  }
+
+  let max = 0;
+  for (const [key, value] of map) {
+    if (map.has(key + 1)) {
+      max = Math.max(max, value + map.get(key + 1));
+    }
+  }
+
+  return max;
+};
