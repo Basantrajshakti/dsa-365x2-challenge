08376582c3e0c4ee8ce72d4774c391cbe0eef5e0
Basantraj.shakti <basantrajshakti@gmail.com>
2026-05-17 23:02:05 +0530

    605. Can Place Flowers

--- dsa/may/17-05-2026.js
+++ dsa/may/17-05-2026.js
@@ -34,3 +34,28 @@ var canReach = function (arr, start) {
 
   return false;
 };
+
+// 605. Can Place Flowers
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+  let i = 0;
+
+  while (n !== 0 && i < flowerbed.length) {
+    if (flowerbed[i] === 1) {
+      i = i + 2;
+      continue;
+    }
+    if (!flowerbed[i - 1] && !flowerbed[i + 1]) {
+      n--;
+      i = i + 2;
+    } else {
+      i++;
+    }
+  }
+
+  return n === 0;
+};
