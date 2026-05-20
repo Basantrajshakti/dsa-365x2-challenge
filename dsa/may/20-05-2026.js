9e1b754dfe7eb5b3eba9eb84d89b145b3ff9fdf2
Basantraj.shakti <basantrajshakti@gmail.com>
2026-05-20 22:17:47 +0530

    645. Set Mismatch

--- dsa/may/20-05-2026.js
+++ dsa/may/20-05-2026.js
@@ -26,3 +26,27 @@ var findThePrefixCommonArray = function (A, B) {
 
   return ans;
 };
+
+// 645. Set Mismatch
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findErrorNums = function (nums) {
+  let n = nums.length;
+  let freq = new Array(n + 1).fill(0);
+
+  for (let num of nums) {
+    freq[num]++;
+  }
+
+  let duplicate = 0;
+  let missing = 0;
+
+  for (let i = 1; i <= n; i++) {
+    if (freq[i] == 2) duplicate = i;
+    if (freq[i] == 0) missing = i;
+  }
+
+  return [duplicate, missing];
+};
