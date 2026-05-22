048c1ddf0daf862b31c49bd05b6e48a752c220ae
Basantraj.shakti <basantrajshakti@gmail.com>
2026-05-22 23:31:23 +0530

    674. Longest Continuous Increasing Subsequence

--- dsa/may/22-05-2026.js
+++ dsa/may/22-05-2026.js
@@ -30,3 +30,24 @@ var search = function (nums, target) {
 
   return -1;
 };
+
+// 674. Longest Continuous Increasing Subsequence
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLengthOfLCIS = function (nums) {
+  if (nums.length === 0) return 0;
+  let count = 1;
+  let max = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > nums[i - 1]) {
+      count++;
+    } else {
+      count = 1;
+    }
+    max = Math.max(max, count);
+  }
+  return max;
+};
