9eb5d246c2febe1c8d23bbf17a9699e3ac5e51b9
Basantraj.shakti <basantrajshakti@gmail.com>
2026-05-23 22:20:38 +0530

    1752. Check if Array Is Sorted and Rotated

---
+++ dsa/may/23-05-2026.js
@@ -0,0 +1,23 @@
+// Daily challenge
+
+// 1752. Check if Array Is Sorted and Rotated
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var check = function (nums) {
+  let n = nums.length;
+  let count = 0;
+
+  for (let i = 1; i < n; i++) {
+    if (nums[i - 1] > nums[i]) {
+      count++;
+    }
+  }
+
+  if (nums[n - 1] > nums[0]) {
+    count++;
+  }
+
+  return count <= 1;
+};
