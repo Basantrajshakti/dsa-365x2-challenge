670f831303680a844196b7baff7cfc7a765af386
Basantraj.shakti <basantrajshakti@gmail.com>
2026-05-31 09:14:19 +0530

    2126. Destroying Asteroids

---
+++ dsa/may/31-05-2026.js
@@ -0,0 +1,21 @@
+// Daily challenge
+
+// 2126. Destroying Asteroids
+/**
+ * @param {number} mass
+ * @param {number[]} asteroids
+ * @return {boolean}
+ */
+var asteroidsDestroyed = function (mass, asteroids) {
+  asteroids.sort((a, b) => a - b);
+
+  for (const asteroid of asteroids) {
+    if (mass < asteroid) {
+      return false;
+    }
+
+    mass += asteroid;
+  }
+
+  return true;
+};
