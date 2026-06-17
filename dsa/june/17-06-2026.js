51f24dedbfa87c9fd06c6bf8db9a4586672994a8
Basantraj.shakti <basantrajshakti@gmail.com>
2026-06-17 21:51:29 +0530

    3614. Process String with Special Operations II

---
+++ dsa/june/17-06-2026.js
@@ -0,0 +1,43 @@
+// Daily challenge
+
+// 3614. Process String with Special Operations II
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {character}
+ */
+var processStr = function (s, k) {
+  let L = 0;
+  for (const c of s) {
+    if (c === "*") {
+      if (L > 0) L--;
+    } else if (c === "#") {
+      L *= 2;
+    } else if (c === "%") {
+      continue;
+    } else {
+      L++;
+    }
+  }
+  if (k >= L) return ".";
+  let p = k;
+  for (let i = s.length - 1; i >= 0; i--) {
+    const c = s[i];
+    if (c === "*") {
+      L++;
+    } else if (c === "#") {
+      if (p >= Math.floor(L / 2)) {
+        p -= Math.floor(L / 2);
+      }
+      L = Math.floor(L / 2);
+    } else if (c === "%") {
+      p = L - 1 - p;
+    } else {
+      if (L === p + 1) {
+        return c;
+      }
+      L--;
+    }
+  }
+  return ".";
+};
