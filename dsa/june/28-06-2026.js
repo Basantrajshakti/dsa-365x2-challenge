da9882285881a66cea25ddc011f0067d791aba39
Basantrajshakti <basantrajshakti@gmail.com>
2026-06-28 20:11:28 +0530

    1846. Maximum Element After Decreasing and Rearranging optimized

--- dsa/june/28-06-2026.js
+++ dsa/june/28-06-2026.js
@@ -17,3 +17,24 @@ var maximumElementAfterDecrementingAndRearranging = function (arr) {
   }
   return arr[arr.length - 1];
 };
+
+// With optimizations
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var maximumElementAfterDecrementingAndRearranging = function (arr) {
+  arr.sort((a, b) => a - b);
+
+  if (arr[0] != 1) {
+    arr[0] = 1;
+  }
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i - 1] < arr[i] - 1) {
+      arr[i] = arr[i - 1] + 1;
+    }
+  }
+
+  return arr[arr.length - 1];
+};
