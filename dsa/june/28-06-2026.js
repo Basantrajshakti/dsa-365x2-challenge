ea0fad96e9d291e8147cd9e14a82e5d3473b6f5c
Basantrajshakti <basantrajshakti@gmail.com>
2026-06-28 20:03:07 +0530

    1846. Maximum Element After Decreasing and Rearranging

---
+++ dsa/june/28-06-2026.js
@@ -0,0 +1,19 @@
+// Daily challenge
+
+// 1846. Maximum Element After Decreasing and Rearranging
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var maximumElementAfterDecrementingAndRearranging = function (arr) {
+  arr.sort((a, b) => a - b);
+  if (arr[0] != 1) {
+    arr[0] = 1;
+  }
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i - 1] < arr[i] - 1) {
+      arr[i] = arr[i - 1] + 1;
+    }
+  }
+  return arr[arr.length - 1];
+};
