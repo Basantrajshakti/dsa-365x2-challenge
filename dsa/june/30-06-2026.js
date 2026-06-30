ba9de42ac11d92e446aa55a700d0d2a23c905286
Basantrajshakti <basantrajshakti@gmail.com>
2026-06-30 21:53:44 +0530

    1358. Number of Substrings Containing All Three Characters

---
+++ dsa/june/30-06-2026.js
@@ -0,0 +1,25 @@
+// Daily challenge
+
+// 1358. Number of Substrings Containing All Three Characters
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var numberOfSubstrings = function (s) {
+  let lastA = -1,
+    lastB = -1,
+    lastC = -1;
+  let total = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "a") lastA = i;
+    else if (s[i] === "b") lastB = i;
+    else lastC = i;
+
+    if (lastA !== -1 && lastB !== -1 && lastC !== -1) {
+      total += Math.min(lastA, lastB, lastC) + 1;
+    }
+  }
+
+  return total;
+};
