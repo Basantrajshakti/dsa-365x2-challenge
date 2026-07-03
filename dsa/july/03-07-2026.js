fdd52663c90e0c7404e920d985586556d7d374a6
Basantrajshakti <basantrajshakti@gmail.com>
2026-07-03 23:27:22 +0530

    3620. Network Recovery Pathways optimized

--- dsa/july/03-07-2026.js
+++ dsa/july/03-07-2026.js
@@ -124,3 +124,64 @@ var findMaxPathScore = function (edges, online, k) {
 
   return right >= 0 ? right : -1;
 };
+
+// With optimizations
+/**
+ * @param {number[][]} edges
+ * @param {boolean[]} online
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxPathScore = function (edges, online, k) {
+  const n = online.length;
+  const g = Array.from({ length: n }, () => []);
+  let l = Infinity,
+    r = 0;
+
+  for (const [u, v, w] of edges) {
+    if (!online[u] || !online[v]) {
+      continue;
+    }
+    g[u].push([v, w]);
+    l = Math.min(l, w);
+    r = Math.max(r, w);
+  }
+
+  const check = (mid) => {
+    const memo = new Array(n).fill(-1);
+
+    const dfs = (u) => {
+      if (u === n - 1) {
+        return 0;
+      }
+      if (memo[u] !== -1) {
+        return memo[u];
+      }
+
+      let res = Infinity;
+      for (const [v, w] of g[u]) {
+        if (w >= mid) {
+          res = Math.min(res, dfs(v) + w);
+        }
+      }
+      memo[u] = res;
+      return res;
+    };
+
+    return dfs(0) <= k;
+  };
+
+  if (!check(l)) {
+    return -1;
+  }
+
+  while (l <= r) {
+    const mid = (l + r) >> 1;
+    if (check(mid)) {
+      l = mid + 1;
+    } else {
+      r = mid - 1;
+    }
+  }
+  return r;
+};
