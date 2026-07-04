fa24550111dc4817cb89b202da1447e2462a912f
Basantrajshakti <basantrajshakti@gmail.com>
2026-07-04 23:45:04 +0530

    2492. Minimum Score of a Path Between Two Cities

---
+++ dsa/july/04-07-2026.js
@@ -0,0 +1,22 @@
+// Daily challenge
+
+// 2492. Minimum Score of a Path Between Two Cities
+/**
+ * @param {number} n
+ * @param {number[][]} roads
+ * @return {number}
+ */
+var minScore = function (n, roads) {
+  const root = [...Array(n + 1).keys()];
+  const find = (i) => (root[i] === i ? i : (root[i] = find(root[i])));
+
+  for (const [x, y, _] of roads) root[find(x)] = find(y);
+
+  roads = roads.filter((r) => find(r[0]) === find(1));
+
+  let min = Infinity;
+
+  for (const [, , d] of roads) min = Math.min(min, d);
+
+  return min;
+};
