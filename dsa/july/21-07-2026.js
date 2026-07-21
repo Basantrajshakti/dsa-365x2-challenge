5b25463bc7ac49ff22d10ee27e7e07b629bbc623
Basantrajshakti <basantrajshakti@gmail.com>
2026-07-21 20:41:53 +0530

    3499. Maximize Active Section with Trade I

---
+++ dsa/july/21-07-2026.js
@@ -0,0 +1,41 @@
+// 3499. Maximize Active Section with Trade I
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxActiveSectionsAfterTrade = function (s) {
+  let ones = 0;
+
+  for (const c of s) {
+    if (c === "1") {
+      ones++;
+    }
+  }
+
+  const paddedS = "1" + s + "1";
+  const zeroRuns = [];
+
+  let length = 0;
+
+  for (const c of paddedS) {
+    if (c === "0") {
+      length++;
+    } else if (length > 0) {
+      zeroRuns.push(length);
+      length = 0;
+    }
+  }
+
+  if (zeroRuns.length < 2) {
+    return ones;
+  }
+
+  let best = 0;
+
+  for (let i = 0; i + 1 < zeroRuns.length; i++) {
+    best = Math.max(best, zeroRuns[i] + zeroRuns[i + 1]);
+  }
+
+  return ones + best;
+};
