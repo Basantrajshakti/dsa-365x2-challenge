cf0ddd77ef7de1ffc7d58064c464ca860d6bac80
Basantrajshakti <basantrajshakti@gmail.com>
2026-07-23 21:16:54 +0530

    3513. Number of Unique XOR Triplets I

---
+++ dsa/july/23-07-2026.js
@@ -0,0 +1,20 @@
+// 3513. Number of Unique XOR Triplets I
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var uniqueXorTriplets = function (nums) {
+  const n = nums.length;
+
+  if (n <= 2) return n;
+
+  let bits = 0;
+  let x = n;
+  while (x > 0) {
+    bits++;
+    x >>= 1;
+  }
+
+  return 1 << bits;
+};
