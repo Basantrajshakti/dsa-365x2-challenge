359d0bbf3b548bdb7b1104e81915cab8a535211d
Basantrajshakti <basantrajshakti@gmail.com>
2026-07-27 19:45:13 +0530

    1464. Maximum Product of Two Elements in an Array

---
+++ dsa/july/27-07-2026.js
@@ -0,0 +1,20 @@
+// 1464. Maximum Product of Two Elements in an Array
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function (nums) {
+  let first = 0;
+  let second = 0;
+
+  for (const num of nums) {
+    if (num >= first) {
+      second = first;
+      first = num;
+    } else if (num > second) {
+      second = num;
+    }
+  }
+
+  return (first - 1) * (second - 1);
+};
