a20881377c86245c023875913578f3e53f0e8695
Basantrajshakti <basantrajshakti@gmail.com>
2026-08-04 20:39:42 +0530

    3731. Find Missing Elements optimized

--- dsa/aug/04-08-2026.js
+++ dsa/aug/04-08-2026.js
@@ -17,3 +17,32 @@ var findMissingElements = function (nums) {
 
   return res;
 };
+
+// Optimized
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findMissingElements = function (nums) {
+  let largest = nums[0];
+  let smallest = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    largest = Math.max(nums[i], largest);
+    smallest = Math.min(nums[i], smallest);
+  }
+
+  let visited = [];
+  for (let ele of nums) {
+    visited[ele] = 1;
+  }
+
+  let result = [];
+  for (let i = smallest; i <= largest; i++) {
+    if (!visited[i]) {
+      result.push(i);
+    }
+  }
+
+  return result;
+};
