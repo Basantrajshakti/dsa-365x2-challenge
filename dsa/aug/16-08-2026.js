d7ce06a6f2eb3b3e3d78d9a63cd4469938869a7e
Basantrajshakti <basantrajshakti@gmail.com>
2026-08-16 18:59:21 +0530

    2029. Stone Game IX

---
+++ dsa/aug/16-08-2026.js
@@ -0,0 +1,18 @@
+// 2029. Stone Game IX
+/**
+ * @param {number[]} stones
+ * @return {boolean}
+ */
+var stoneGameIX = function (stones) {
+  const count = [0, 0, 0];
+
+  for (const stone of stones) {
+    count[stone % 3]++;
+  }
+
+  if (count[0] % 2 === 0) {
+    return count[1] > 0 && count[2] > 0;
+  }
+
+  return Math.abs(count[1] - count[2]) >= 3;
+};
