c4975b811093298bc59d586fd7022a927b227932
Basantrajshakti <basantrajshakti@gmail.com>
2026-09-03 20:27:40 +0530

    3876. Construct Uniform Parity Array II Optimized

--- dsa/sept/03-09-2026.js
+++ dsa/sept/03-09-2026.js
@@ -17,3 +17,23 @@ var uniformArray = function (nums1) {
   }
   return true;
 };
+
+// With optimizations
+/**
+ * @param {number[]} nums1
+ * @return {boolean}
+ */
+var uniformArray = function (nums1) {
+  let mn = nums1[0];
+  let hasOdd = false;
+
+  for (const v of nums1) {
+    if (v < mn) mn = v;
+
+    if (v & 1) hasOdd = true;
+  }
+
+  if (mn & 1) return true;
+
+  return !hasOdd;
+};
