0eeeb6541679befa0baaeeddac46e613f82314a1
Basantrajshakti <basantrajshakti@gmail.com>
2026-09-05 22:08:45 +0530

    3904. Smallest Stable Index II

---
+++ dsa/sept/05-09-2026.js
@@ -0,0 +1,21 @@
+// 3904. Smallest Stable Index II
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var firstStableIndex = function (nums, k) {
+  let maxSoFar = -1;
+  let cand = 0,
+    max = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    maxSoFar = Math.max(maxSoFar, nums[i]);
+
+    if (cand === i) max = maxSoFar;
+
+    if (nums[i] < max - k) cand = i + 1;
+  }
+
+  return cand < nums.length ? cand : -1;
+};
