579d5250af4c0339d8ee48177f00b7b4dd6ec847
Basantrajshakti <basantrajshakti@gmail.com>
2026-09-08 21:40:50 +0530

    3870. Count Commas in Range

---
+++ dsa/sept/08-09-2026.js
@@ -0,0 +1,17 @@
+// 3870. Count Commas in Range
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countCommas = function (n) {
+  let answer = 0;
+  let threshold = 1000;
+
+  while (threshold <= n) {
+    answer += n - threshold + 1;
+
+    threshold *= 1000;
+  }
+
+  return answer;
+};
