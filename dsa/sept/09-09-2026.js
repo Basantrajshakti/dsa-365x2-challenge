aa9ce0d5210438fc3ffe4cf8fe453dab35a104af
Basantrajshakti <basantrajshakti@gmail.com>
2026-09-09 18:55:33 +0530

    3871. Count Commas in Range II

---
+++ dsa/sept/09-09-2026.js
@@ -0,0 +1,24 @@
+// 3871. Count Commas in Range II
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countCommas = function (n) {
+  let start = 1000;
+  let commas = 1;
+  let answer = 0;
+
+  while (start <= n) {
+    let end;
+
+    if (start > n / 1000) end = n;
+    else end = start * 1000 - 1;
+
+    const count = end - start + 1;
+    answer += count * commas;
+    start *= 1000;
+    commas++;
+  }
+
+  return answer;
+};
