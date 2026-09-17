3154efbb3597f6a6aec40d6451464f7ccf8a40c4
Basantrajshakti <basantrajshakti@gmail.com>
2026-09-17 20:22:12 +0530

    1477. Find Two Non-overlapping Sub-arrays Each With Target Sum

---
+++ dsa/sept/17-09-2026.js
@@ -0,0 +1,29 @@
+// 1477. Find Two Non-overlapping Sub-arrays Each With Target Sum
+/**
+ * @param {number[]} arr
+ * @param {number} target
+ * @return {number}
+ */
+var minSumOfLengths = function (arr, target) {
+  let n = arr.length,
+    res = n + 1;
+  let sum = 0,
+    i = 0;
+
+  let dp = new Int32Array(n + 1).fill(n);
+
+  for (let j = 0; j < n; j++) {
+    sum += arr[j];
+
+    while (sum > target) sum -= arr[i++];
+
+    dp[j + 1] = dp[j];
+
+    if (sum === target) {
+      res = Math.min(res, j - i + 1 + dp[i]);
+      dp[j + 1] = Math.min(dp[j], j - i + 1);
+    }
+  }
+
+  return res === n + 1 ? -1 : res;
+};
