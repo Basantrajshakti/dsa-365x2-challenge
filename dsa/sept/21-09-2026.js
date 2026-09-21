59f8bea97f63fff8d7b3b76044bbe34ef2c1773b
Basantrajshakti <basantrajshakti@gmail.com>
2026-09-21 20:52:19 +0530

    3524. Find X Value of Array I

---
+++ dsa/sept/21-09-2026.js
@@ -0,0 +1,27 @@
+// 3524. Find X Value of Array I
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var resultArray = function (nums, k) {
+  const result = new Array(k).fill(0);
+  let state = new Array(k).fill(0);
+
+  for (const value of nums) {
+    const rem = value % k;
+    const nextState = new Array(k).fill(0);
+
+    for (let r = 0; r < k; r++) {
+      const newRem = (r * rem) % k;
+      nextState[newRem] += state[r];
+      result[newRem] += state[r];
+    }
+
+    nextState[rem]++;
+    result[rem]++;
+    state = nextState;
+  }
+
+  return result;
+};
