4b512e210054175b2a3a98d9173ad4b538bdb3c6
Basantrajshakti <basantrajshakti@gmail.com>
2026-09-25 22:06:52 +0530

    1096. Brace Expansion II optimized

--- dsa/sept/25-09-2026.js
+++ dsa/sept/25-09-2026.js
@@ -61,3 +61,60 @@ var braceExpansionII = function (expression) {
 
   return dfs(0, expression.length - 1).sort();
 };
+
+// With optimizations
+/**
+ * @param {string} expression
+ * @return {string[]}
+ */
+var braceExpansionII = function (expression) {
+  const op = [];
+  const stk = [];
+
+  const ope = () => {
+    const l = stk.length - 2;
+    const r = stk.length - 1;
+
+    if (op[op.length - 1] === "+") {
+      for (const item of stk[r]) stk[l].add(item);
+    } else {
+      const tmp = new Set();
+
+      for (const left of stk[l]) {
+        for (const right of stk[r]) {
+          tmp.add(left + right);
+        }
+      }
+
+      stk[l] = tmp;
+    }
+
+    op.pop();
+    stk.pop();
+  };
+
+  for (let i = 0; i < expression.length; i++) {
+    const ch = expression[i];
+    if (ch === ",") {
+      while (op.length > 0 && op[op.length - 1] === "*") ope();
+
+      op.push("+");
+    } else if (ch === "{") {
+      if (i > 0 && (expression[i - 1] === "}" || /[a-z]/.test(expression[i - 1]))) op.push("*");
+
+      op.push("{");
+    } else if (ch === "}") {
+      while (op.length > 0 && op[op.length - 1] !== "{") ope();
+
+      op.pop();
+    } else {
+      if (i > 0 && (expression[i - 1] === "}" || /[a-z]/.test(expression[i - 1]))) op.push("*");
+
+      stk.push(new Set([ch]));
+    }
+  }
+
+  while (op.length > 0) ope();
+
+  return Array.from(stk[stk.length - 1]).sort();
+};
